84836aa8569c0b23ad88a2f3cb80be03f9a78f0d
Anu Viswan <anu.viswan@gmail.com>
2019-10-06 10:17:43 +0530

    Working on RepositoryService

---
+++ nt.webapi/src/Nt.WebApi/Models/MovieDto.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using Nt.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nt.WebApi.Models
+{
+    public class MovieDto : BaseDto
+    {
+        [BsonElement("movieTitle")]
+        public string Title { get; set; }
+        [BsonElement("releaseDate")]
+        public DateTime ReleaseDate { get; set; }
+        [BsonElement("directorName")]
+        public string DirectorName { get; set; }
+    }
+}
---
+++ nt.webapi/src/Nt.WebApi/Services/RepositoryService.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using Nt.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nt.WebApi.Services
+{
+    public class RepositoryService<T> where T :BaseDto
+    {
+        private readonly IMongoCollection<T> _dataCollection;
+
+        public RepositoryService(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _dataCollection = database.GetCollection<T>(settings.CollectionName);
+        }
+        public T Create(T dto)
+        {
+            _dataCollection.InsertOne(dto);
+            return dto;
+        }
+
+    }
+}
