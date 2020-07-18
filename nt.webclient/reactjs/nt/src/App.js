2743f7cb26be9c114c3e0ac1f2863bfb647166a0
Anu Viswan <anu.viswan@gmail.com>
2020-07-18 19:22:24 +0530

    Adding login file

--- nt.webclient/reactjs/nt/src/App.js
+++ nt.webclient/reactjs/nt/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Login from "./components/Pages/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/Pages/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Navbar title='November Talkies' icon='fa fa-github' />
         <Switch>
           <Route exact path='/' render={() => <Login />} />
+          <Route exact path='/about' component={About} />
         </Switch>
       </div>
     </Router>
