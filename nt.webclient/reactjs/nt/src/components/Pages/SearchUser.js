bbfda358267f86089ea2b3882a877a07f9cf3553
Anu Viswan <anu.viswan@gmail.com>
2020-07-20 14:32:11 +0530

    Working on Search API

--- nt.webclient/reactjs/nt/src/components/Pages/SearchUser.js
+++ nt.webclient/reactjs/nt/src/components/Pages/SearchUser.js
@@ -1,25 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import UserSearchBar from "../User/UserSearchBar";
 import axios from "axios";
 
-class SearchUser extends Component {
-  searchForUsers = async (text) => {
+const SearchUser = () => {
+  const [users, setUsers] = useState([{ displayName: "", userName: "" }]);
+
+  const searchForUsers = async (text) => {
     const res = await axios.get(
       `https://localhost:44353/api/User/SearchUser?partialString=${text}`
     );
-    this.setState({ users: res.data });
-    console.log(this.state.users);
+    setUsers(res.data);
+    console.log(res.data);
+    console.log(users);
   };
-  render() {
-    return (
+
+  return (
+    <div>
       <div>
-        <div>
-          <UserSearchBar searchUsers={this.searchForUsers} />
-        </div>
-        <div>TODO:Search Result Page</div>
+        <UserSearchBar searchUsers={searchForUsers} />
       </div>
-    );
-  }
-}
+      <div>TODO:Search Result Page</div>
+    </div>
+  );
+};
 
 export default SearchUser;
