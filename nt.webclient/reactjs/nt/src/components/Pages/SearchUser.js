6979a7972458556475926b8d07f56c5f7b225c7f
Anu Viswan <anu.viswan@gmail.com>
2020-07-20 13:59:31 +0530

    Call to Search API

--- nt.webclient/reactjs/nt/src/components/Pages/SearchUser.js
+++ nt.webclient/reactjs/nt/src/components/Pages/SearchUser.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import UserSearchBar from "../User/UserSearchBar";
 import axios from "axios";
+import { render } from "@testing-library/react";
 
-const SearchUser = () => {
-  const searchUsers = async (text) => {
-    console.log(text);
+class SearchUser extends Component {
+  state = {
+    users: {},
   };
-  return (
-    <div>
+  searchForUsers = async (text) => {
+    const res = await axios.get(
+      `https://localhost:44353/api/User/SearchUser?partialString=${text}`
+    );
+    this.setState({ users: res.data });
+    console.log(res.data);
+    console.log(this.users);
+  };
+  render() {
+    return (
       <div>
-        <UserSearchBar searchUsers={searchUsers} />
+        <div>
+          <UserSearchBar searchUsers={this.searchForUsers} />
+        </div>
+        <div>Search Result Page</div>
       </div>
-      <div>Search Result Page</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SearchUser;
