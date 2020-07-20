ab5a3b871cc2c55f8484b936a11e1ff9eba25780
Anu Viswan <anu.viswan@gmail.com>
2020-07-20 06:36:10 +0530

    Added unit test for controller and service (Search)

--- nt.webapi/src/Domain/Nt.Domain.RepositoryContracts/User/IUserProfileRepository.cs
+++ nt.webapi/src/Domain/Nt.Domain.RepositoryContracts/User/IUserProfileRepository.cs
@@ -3,9 +3,8 @@ using System.Threading.Tasks;
 
 namespace Nt.Domain.RepositoryContracts.User
 {
-    public interface IUserProfileRepository
+    public interface IUserProfileRepository:IGenericRepository<UserProfileEntity>
     {
-
         Task<UserProfileEntity> ValidateUserAsync(string userName, string passKey);
         Task<bool> CheckIfUserExistsAsync(string userName);
     }
