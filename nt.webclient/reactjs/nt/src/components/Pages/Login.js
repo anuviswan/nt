2eea3340b74c3e5098cfef7c58a9026e97fb343d
Anu Viswan <anu.viswan@gmail.com>
2020-07-26 05:36:30 +0530

    Implementing authentication

--- nt.webclient/reactjs/nt/src/components/Pages/Login.js
+++ nt.webclient/reactjs/nt/src/components/Pages/Login.js
@@ -1,23 +1,56 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const initialProfileValue = {
+    userName: "",
+    passKey: "",
+  };
+  const [userProfile, setLoginProfile] = useState(initialProfileValue);
+
+  const onChange = async (e) => {
+    setLoginProfile({ ...userProfile, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const headers = {
+      "Access-Control-Allow-Headers": "*", // this will allow all CORS requests
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET", // this states the allowed methods
+      "Content-Type": "application/json", // this shows the expected content type
+    };
+
+    const result = await axios.post(
+      "https://localhost:44353/api/User/ValidateUser",
+      userProfile,
+      { headers: headers }
+    );
+    console.log(result);
+  };
+
   return (
-    <div class='container'>
-      <div class='col-md-12 min-vh-100 d-flex flex-column justify-content-center'>
-        <div class='row'>
-          <div class='col-lg-6 col-md-8 mx-auto'>
-            <div class='card rounded shadow shadow-sm'>
-              <div class='card-header'>
-                <h3 class='mb-0'>Login</h3>
+    <div className='container'>
+      <div className='col-md-12 min-vh-100 d-flex flex-column justify-content-center'>
+        <div className='row'>
+          <div className='col-lg-6 col-md-8 mx-auto'>
+            <div className='card rounded shadow shadow-sm'>
+              <div className='card-header'>
+                <h3 className='mb-0'>Sign In</h3>
               </div>
-              <div class='card-body'>
-                <form className='form'>
-                  <input type='text' name='username' placeholder='UserName' />
+              <div className='card-body'>
+                <form className='form needs-validation' onSubmit={onSubmit}>
+                  <input
+                    type='text'
+                    name='userName'
+                    placeholder='UserName'
+                    onChange={onChange}
+                  />
                   <input
                     type='password'
-                    name='password'
+                    name='passKey'
                     placeholder='Password'
+                    onChange={onChange}
                   />
                   <input type='submit' value='Login' />
                 </form>
