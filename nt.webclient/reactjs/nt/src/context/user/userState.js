d6f9c835b664f126fd778df667f9b4ab04476f39
Anu Viswan <anu.viswan@gmail.com>
2020-07-27 18:03:30 +0530

    renamed

---
+++ nt.webclient/reactjs/nt/src/context/user/userState.js
@@ -0,0 +1,41 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import UserContext from "./userContext";
+import UserReducer from "./userReducer";
+import { SET_CURRENT_USER } from "../types";
+
+const UserState = (props) => {
+  const initialState = {
+    currentUser: {},
+    userToken: "",
+    isUserAuthenticated: false,
+  };
+
+  const [state, dispatch] = useReducer(UserReducer, initialState);
+
+  // search user
+
+  // validate user
+  const validateUser = async (userProfile) => {
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: userProfile,
+    });
+  };
+
+  // register user
+
+  return (
+    <UserContext.Provider
+      value={{
+        currentUser: state.currentUser,
+        userToken: state.userToken,
+        validateUser,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserState;
