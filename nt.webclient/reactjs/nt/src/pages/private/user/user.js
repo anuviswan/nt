af6ed663c4cb151735f837a7920fb9bc0df99a5e
Anu Viswan <anu.viswan@gmail.com>
2020-07-29 06:52:36 +0530

    Passed props from navbar.js

--- nt.webclient/reactjs/nt/src/pages/private/user/user.js
+++ nt.webclient/reactjs/nt/src/pages/private/user/user.js
@@ -1,13 +1,15 @@
 import React from "react";
 import UserCard from "../../../components/User/userCard";
-const User = () => {
+const User = ({ location }) => {
+  console.log(location);
+  const { user } = location.state;
   return (
     <div className='row'>
       <div className='col-md-4 col-lg-2 col-lx-2'>
         <UserCard
           user={{
-            displayName: "Jia Anu",
-            userName: "jiaanu",
+            displayName: user.userName,
+            userName: user.userName,
             bio: "Hey, am jia kutty",
             rating: 4,
           }}
@@ -15,11 +17,11 @@ const User = () => {
         />
       </div>
       <div className='col-md-8 col-lg-10 col-lx-10'>
-        <div class='card'>
-          <div class='card-header'>Movie Reviews</div>
-          <div class='card-body'>
-            <h5 class='card-title'>Dil Bechara</h5>
-            <p class='card-text text-wrap'>
+        <div className='card'>
+          <div className='card-header'>Movie Reviews</div>
+          <div className='card-body'>
+            <h5 className='card-title'>Dil Bechara</h5>
+            <p className='card-text text-wrap'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
               nec placerat neque. Praesent ipsum sem, posuere at tortor sed,
               pharetra vestibulum metus. Suspendisse id porttitor tortor.
@@ -27,13 +29,13 @@ const User = () => {
               purus vel mauris laoreet tempus eu non turpis. Nam in porttitor
               erat. Mauris non lorem mi. Praesent ac hendrerit quam.
             </p>
-            <a href='#' class='btn btn-info'>
+            <a href='#' className='btn btn-info'>
               more
             </a>
           </div>
-          <div class='card-body'>
-            <h5 class='card-title'>Life of Pie</h5>
-            <p class='card-text'>
+          <div className='card-body'>
+            <h5 className='card-title'>Life of Pie</h5>
+            <p className='card-text'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
               nec placerat neque. Praesent ipsum sem, posuere at tortor sed,
               pharetra vestibulum metus. Suspendisse id porttitor tortor.
@@ -41,7 +43,7 @@ const User = () => {
               purus vel mauris laoreet tempus eu non turpis. Nam in porttitor
               erat. Mauris non lorem mi. Praesent ac hendrerit quam.
             </p>
-            <a href='#' class='btn btn-info'>
+            <a href='#' className='btn btn-info'>
               Go somewhere
             </a>
           </div>
