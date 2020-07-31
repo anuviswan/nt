04cb6946b33351449c49ad7face56a9020bd2022
Anu Viswan <anu.viswan@gmail.com>
2020-07-31 12:56:12 +0530

    Working on user edit page

--- nt.webclient/reactjs/nt/src/pages/private/user/editUser.js
+++ nt.webclient/reactjs/nt/src/pages/private/user/editUser.js
@@ -1,10 +1,37 @@
 import React from "react";
 const EditUser = () => {
   return (
-    <div class='container'>
-      <form className='form needs-validation'>
-        <input type='text' name='userName' placeholder='UserName' />
-      </form>
+    <div className='container'>
+      <div className='row'>
+        <div>
+          <ul class='list-group'>
+            <li class='list-group-item'>Edit User Profile</li>
+            <li class='list-group-item'>Change Password</li>
+          </ul>
+        </div>
+        <div className='col-lg-8'>
+          <div className='card'>
+            <div className='card-header'>Featured</div>
+            <div className='card-body'>
+              <form className='form needs-validation'>
+                <p className='font-weight-bold'>User Name</p>
+                <input
+                  type='text'
+                  name='userName'
+                  placeholder='UserName'
+                  readonly
+                />
+
+                <p className='font-weight-bold'>Display Name</p>
+                <input type='text' name='userName' placeholder='UserName' />
+
+                <p className='font-weight-bold'>Bio</p>
+                <input type='text' name='userName' placeholder='UserName' />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
