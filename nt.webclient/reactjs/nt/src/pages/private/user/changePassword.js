61b5101e4f40366c30601e51a7d9f7444472e0f8
Anu Viswan <anu.viswan@gmail.com>
2020-08-03 06:05:58 +0530

    Working on changePassword.js Separated EditUserMenu

--- nt.webclient/reactjs/nt/src/pages/private/user/changePassword.js
+++ nt.webclient/reactjs/nt/src/pages/private/user/changePassword.js
@@ -1,14 +1,14 @@
-import React, { useState, useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Axios from "axios";
 import UserContext from "../../../context/user/userContext";
 import ValidationMessage from "../../../components/layout/validationMessage";
 import EditUserMenu, {
-  EDIT_PROFILE,
+  CHANGE_PWD,
 } from "../../../components/User/editUserMenu";
 
-const EditUser = ({ location }) => {
+const ChangePassword = ({ location }) => {
   const userContext = useContext(UserContext);
   const authToken = userContext.userToken;
 
@@ -19,79 +19,47 @@ const EditUser = ({ location }) => {
   });
 
   const { user } = location.state;
-  const [userDetails, setUserDetails] = useState(user);
+  const [userDetails, setUserDetails] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
 
-  const onChange = (e) => {
-    setUserDetails({ ...userDetails, [e.target.name]: e.target.value });
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    const headers = {
-      "Access-Control-Allow-Headers": "*", // this will allow all CORS requests
-      "Access-Control-Allow-Methods": "OPTIONS,POST,GET", // this states the allowed methods
-      "Content-Type": "application/json", // this shows the expected content type
-      Authorization: `Bearer ${authToken}`,
-    };
-
-    const response = await Axios.post(
-      "https://localhost:44353/api/User/UpdateUser",
-      userDetails,
-      { headers: headers }
-    );
-
-    validateResponse(response.data.errorMessage);
-  };
-
-  const validateResponse = (errorMessage) => {
-    if (errorMessage) {
-      setFormValidation({
-        isValid: false,
-        isVisible: true,
-        message: errorMessage,
-      });
-    } else {
-      setFormValidation({
-        isValid: true,
-        isVisible: true,
-        message: "User Details updated successfully",
-      });
-    }
-  };
+  const onChange = (e) => {};
+  const onSubmit = (e) => {};
 
   return (
     <div className='container-fluid '>
       <div className='row'>
         <div className='col-lg-2'>
-          <EditUserMenu user={user} selected={EDIT_PROFILE} />
+          <EditUserMenu user={user} selected={CHANGE_PWD} />
         </div>
         <div className='col-lg-10'>
           <div className='card'>
-            <div className='card-header'>Edit User Profile</div>
+            <div className='card-header'>Change Password</div>
             <div className='card-body'>
               <form className='form needs-validation' onSubmit={onSubmit}>
-                <p className='font-weight-bold'>User Name</p>
+                <p className='font-weight-bold'>Old Password</p>
                 <input
                   type='text'
-                  value={userDetails.userName}
+                  value={userDetails.oldPassword}
                   name='userName'
                   readOnly
                 />
 
-                <p className='font-weight-bold'>Display Name</p>
+                <p className='font-weight-bold'>New Password</p>
                 <input
                   type='text'
                   name='displayName'
-                  value={userDetails.displayName}
+                  value={userDetails.newPassword}
                   onChange={onChange}
                 />
 
-                <p className='font-weight-bold'>Bio</p>
+                <p className='font-weight-bold'>Confirm Password</p>
                 <input
                   type='text'
                   name='bio'
-                  value={userDetails.bio}
+                  value={userDetails.confirmPassword}
                   onChange={onChange}
                 />
 
@@ -110,8 +78,8 @@ const EditUser = ({ location }) => {
   );
 };
 
-EditUser.propTypes = {
+ChangePassword.propTypes = {
   location: PropTypes.object.isRequired,
 };
 
-export default EditUser;
+export default ChangePassword;
