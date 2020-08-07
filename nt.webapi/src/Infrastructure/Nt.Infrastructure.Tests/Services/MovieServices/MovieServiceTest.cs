c5e9961ecc7203d33e02b82dd8894edac77744c5
Anu Viswan <anu.viswan@gmail.com>
2020-08-07 05:50:18 +0530

    Added Unit Test for MovieServiceTest : Failure Cases

--- nt.webapi/src/Infrastructure/Nt.Infrastructure.Tests/Services/MovieServices/MovieServiceTest.cs
+++ nt.webapi/src/Infrastructure/Nt.Infrastructure.Tests/Services/MovieServices/MovieServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using Nt.Application.Services.Movie;
+using Nt.Domain.Entities.Exceptions;
 using Nt.Domain.Entities.Movie;
 using Nt.Domain.RepositoryContracts;
 using Nt.Domain.RepositoryContracts.Movie;
@@ -70,6 +71,52 @@ namespace Nt.Infrastructure.Tests.Services.MovieServices
             {
                 new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddYears(-1), Language = "Malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" } },
                 new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddYears(-1), Language = "malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" } , Id = "RandomId"}
+            },
+        };
+
+
+        [Theory]
+        [MemberData(nameof(CreateMovieFailureTestData))]
+        public async Task CreateMovieFailureTest(MovieEntity request, MovieEntity expected)
+        {
+            // Arrange
+            var mockMovieRepository = new Mock<IMovieRepository>();
+            mockMovieRepository.Setup(x => x.CreateAsync(It.IsAny<MovieEntity>())).Returns(Task.FromResult(expected));
+            mockMovieRepository.Setup(x => x.GetAsync(It.IsAny<Expression<Func<MovieEntity, bool>>>()))
+                .Returns(Task.FromResult(EntityCollection.Where(x => x.Title.ToLower().Equals(request.Title.ToLower())
+            && x.ReleaseDate.Year == request.ReleaseDate.Year
+            && x.Language.ToLower().Equals(request.Language.ToLower()))));
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.SetupGet(x => x.MovieRepository).Returns(mockMovieRepository.Object);
+
+
+            
+            //Act
+            var movieService = new MovieService(mockUnitOfWork.Object);
+            await Assert.ThrowsAsync<EntityAlreadyExistException>(()=>movieService.CreateAsync(request));
+            
+            // Assert
+            Assert.Equal(expected.Title, request.Title);
+            mockMovieRepository.Verify(x => x.CreateAsync(It.IsAny<MovieEntity>()), Times.Never);
+            mockMovieRepository.Verify(x => x.GetAsync(It.IsAny<Expression<Func<MovieEntity, bool>>>()), Times.Once);
+        }
+
+        public static IEnumerable<object[]> CreateMovieFailureTestData => new []
+        {
+            new[]
+            {
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now, Language = "Malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" } },
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now, Language = "Malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" }}
+            },
+            new[]
+            {
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddMonths(1), Language = "Malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" } },
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddMonths(1), Language = "Malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" }}
+            },
+                        new[]
+            {
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddMonths(1), Language = "malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" } },
+                new MovieEntity { Title = "Movie Sample 1", ReleaseDate = DateTime.Now.AddMonths(1), Language = "malayalam", Actors = new List<string>{ "Actor 1", "Actor 2" }}
             },
         };
     }
