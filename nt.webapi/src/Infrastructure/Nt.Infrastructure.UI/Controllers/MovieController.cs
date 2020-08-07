816b9b003e3ba3fc2b74539e3e73c89d3b400810
Anu Viswan <anu.viswan@gmail.com>
2020-08-07 07:05:26 +0530

    Created Unit Tests for CreateMovie Controller Action

---
+++ nt.webapi/src/Infrastructure/Nt.Infrastructure.UI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Nt.Domain.ServiceContracts.Movie;
+using Nt.Infrastructure.WebApi.ViewModels.Areas.Movie.CreateMovie;
+using System;
+using System.Threading.Tasks;
+
+namespace Nt.Infrastructure.WebApi.Controllers
+{
+    public class MovieController:BaseController
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMapper mapper, IMovieService movieService) : base(mapper) => _movieService = movieService;
+
+        public Task<CreateMovieResponse>  CreateMovie(CreateMovieRequest movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ nt.webapi/src/Infrastructure/Nt.Infrastructure.UI/ViewModels/Areas/Movie/CreateMovie/CreateMovieRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nt.Infrastructure.WebApi.ViewModels.Areas.Movie.CreateMovie
+{
+    public class CreateMovieRequest
+    {
+        public string Title { get; set; }
+        public string Language { get; set; }
+        public DateTime ReleaseDate{get;set;}
+        public List<string> Actors { get; set; }
+    }
+}
