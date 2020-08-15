68bef55595380bea66a57c30fbcd77b7a5b3279d
Anu Viswan <anu.viswan@gmail.com>
2020-08-15 11:10:25 +0530

    creating boiler plate code

--- nt.webclient/vuejs/nt/src/App.vue
+++ nt.webclient/vuejs/nt/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-danger">
+      <i class="fas fa-5x fa-video" />
+    </div>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./pages/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
---
+++ nt.webclient/vuejs/nt/src/pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-lg-9">
+      <h1>November Talkies</h1>
+    </div>
+    <div class="col-lg-3">
+      <Login />
+    </div>
+  </div>
+</template>
+
+<script>
+import Login from "../components/Login";
+export default {
+  name: "Home",
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style>
+</style>
