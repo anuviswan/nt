4df9b3f02484e8cb39804ec7f72028b6c8626409
Anu Viswan <anu.viswan@gmail.com>
2020-08-30 09:21:53 +0530

    Adding Navbar.vue

---
+++ nt.webclient/vuejs/nt/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top py-0">
+    <a class="navbar-brand" href="#">November Talkies</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">
+            Home
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Users</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Movies</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scopped>
+.navbar {
+  padding-top: 0;
+  padding-bottom: 0;
+}
+</style>
--- nt.webclient/vuejs/nt/src/pages/private/Dashboard.vue
+++ nt.webclient/vuejs/nt/src/pages/private/Dashboard.vue
@@ -1,10 +1,16 @@
 <template>
-  <h1>User has successfully logged in, This is Dashboard</h1>
+  <div>
+    <Navbar />
+    <h1>User has successfully logged in, This is Dashboard</h1>
+  </div>
 </template>
 
 <script>
+import Navbar from "../../components/Navbar";
+
 export default {
   name: "Dashboard",
+  components: { Navbar },
 };
 </script>
 
