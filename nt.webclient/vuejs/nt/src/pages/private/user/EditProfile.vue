e458906a14fc051598a0bea360b9a544df53557f
Anu Viswan <anu.viswan@gmail.com>
2020-10-03 04:37:24 +0530

    Working on layout of EditUserMenu.vue

--- nt.webclient/vuejs/nt/src/pages/private/user/EditProfile.vue
+++ nt.webclient/vuejs/nt/src/pages/private/user/EditProfile.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>Edit User</div>
+  <div className="container-fluid ">
+    <div className="row">
+      <div className="col-sm-2 col-md-2 col-lg-2">
+        <EditUserMenu />
+      </div>
+      <div className="col-sm-8 col-md-8 col-lg-10">
+        The main contend would be displayed here
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import EditUserMenu from "../../../components/user/EditUserMenu";
 export default {
-  name: "EditUser",
+  name: "EditProfile",
+  components: {
+    EditUserMenu,
+  },
 };
 </script>
 
