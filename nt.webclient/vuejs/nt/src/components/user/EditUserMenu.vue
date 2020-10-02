12844c7d39eb8b760b3b151ce72ffb7ae347afcf
Anu Viswan <anu.viswan@gmail.com>
2020-10-03 04:37:48 +0530

    added EditUserMenu.vue

---
+++ nt.webclient/vuejs/nt/src/components/user/EditUserMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul className="list-group">
+    <li>
+      <router-link to="/edit" class="list-group-item">Edit Profile</router-link>
+    </li>
+    <li>
+      <router-link to="/chpwd" class="list-group-item list-group-item-secondary"
+        >Change Password</router-link
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "EditUserMenu",
+};
+</script>
+
+<style scoped>
+ui {
+  list-style-type: none;
+}
+
+li {
+  list-style: none;
+}
+</style>
