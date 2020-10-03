9d5073acbe4d1cca6a7814fffd5cd81ff7f67e99
Anu Viswan <anu.viswan@gmail.com>
2020-10-04 02:21:50 +0530

    updated ui

--- nt.webclient/vuejs/nt/src/pages/private/user/EditProfile.vue
+++ nt.webclient/vuejs/nt/src/pages/private/user/EditProfile.vue
@@ -5,7 +5,35 @@
         <EditUserMenu />
       </div>
       <div class="col-sm-8 col-md-8 col-lg-10">
-        <div>Edit User Profile Details here</div>
+        <div class="card">
+          <div class="card-header font-weight-bold text-uppercase">
+            Edit User Profile
+          </div>
+          <div class="card-body">
+            <form class="form needs-validation">
+              <div class="form-group">
+                <label for="userName">User Name</label>
+                <input
+                  type="text"
+                  v-model="userName"
+                  name="userName"
+                  readOnly
+                />
+              </div>
+              <div class="form-group">
+                <label for="displayName">Display Name</label>
+                <input type="text" v-model="displayName" name="displayName" />
+              </div>
+              <div class="form-group">
+                <label for="bio">Bio</label>
+                <textarea v-model="bio" name="bio" />
+              </div>
+              <div class="form-group">
+                <input type="submit" value="Update" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,8 +46,35 @@ export default {
   components: {
     EditUserMenu,
   },
+  data() {
+    return {
+      userName: "sampleUserName",
+      displayName: "sample Display Name",
+      bio: "sample Bio",
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+input[type="text"],
+input[type="email"],
+input[type="password"],
+input[type="date"],
+select,
+textarea {
+  display: block;
+  width: 100%;
+  padding: 0.4rem;
+  font-size: 1.2rem;
+  border: 1px solid #ccc;
+}
+
+.form-group {
+  align-items: left;
+}
+.form-group label {
+  text-align: left;
+  float: left;
+}
 </style>
