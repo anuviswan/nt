40163265be1c883d8f5cd84a58f56b3766357355
Anu Viswan <anu.viswan@gmail.com>
2020-10-06 05:24:11 +0530

    working on ChangePassword.vue

--- nt.webclient/vuejs/nt/src/pages/private/user/ChangePassword.vue
+++ nt.webclient/vuejs/nt/src/pages/private/user/ChangePassword.vue
@@ -5,7 +5,62 @@
         <EditUserMenu />
       </div>
       <div class="col-sm-8 col-md-8 col-lg-10">
-        <div>Change Password here</div>
+        <div className="card">
+          <div className="card-header">
+            <div class="card-header font-weight-bold text-uppercase">
+              Change Password
+            </div>
+          </div>
+          <div className="card-body">
+            <form className="form needs-validation" v-on:submit="onSubmit">
+              <div class="form-group">
+                <label for="oldPassword">Old Password</label>
+                <input
+                  type="password"
+                  v-model="oldPassword"
+                  name="oldPassword"
+                  v-bind:class="
+                    hasError('oldPassword')
+                      ? 'form-control block is-invalid'
+                      : 'form-control block'
+                  "
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="newPassword">New Password</label>
+                <input
+                  type="password"
+                  name="newPassword"
+                  v-model="newPassword"
+                  v-bind:class="
+                    hasError('newPassword')
+                      ? 'form-control block is-invalid'
+                      : 'form-control block'
+                  "
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="confirmPassword">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  v-model="confirmPassword"
+                  v-bind:class="
+                    hasError('confirmPassword')
+                      ? 'form-control block is-invalid'
+                      : 'form-control block'
+                  "
+                />
+              </div>
+
+              <div class="form-group">
+                <input type="submit" value="Update Password" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -15,11 +70,49 @@
 import EditUserMenu from "../../../components/user/EditUserMenu.vue";
 export default {
   name: "ChangePassword",
+  data() {
+    return {
+      oldPassword: "",
+      newPassword: "",
+      confirmPassword: "",
+      errors: [],
+    };
+  },
   components: {
     EditUserMenu,
+  },
+  methods: {
+    hasError(key) {
+      return this.errors.indexOf(key) != -1;
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+input[type="text"],
+input[type="email"],
+input[type="password"],
+input[type="date"],
+select,
+textarea {
+  display: block;
+  width: 100%;
+  padding: 0.4rem;
+  font-size: 1.2rem;
+  border: 1px solid #ccc;
+}
+
+.form-group {
+  align-items: left;
+}
+.form-group label {
+  text-align: left;
+  float: left;
+}
+
+.form-group small {
+  text-align: center;
+  float: none;
+}
 </style>
