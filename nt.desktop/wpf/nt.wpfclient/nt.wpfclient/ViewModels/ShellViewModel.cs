125976134bc4dcf4c74e0b77e228b72669e4c797
Anu Viswan <anu.viswan@gmail.com>
2020-10-08 04:51:41 +0530

    dummy change

--- nt.desktop/wpf/nt.wpfclient/nt.wpfclient/ViewModels/ShellViewModel.cs
+++ nt.desktop/wpf/nt.wpfclient/nt.wpfclient/ViewModels/ShellViewModel.cs
@@ -32,9 +32,7 @@ namespace Nt.WpfClient.ViewModels
             }
 
             Navbar = IoC.Get<NavbarControl>().ViewModel;
-           
         }
-
         
         private void InvokeLogin()
         {
