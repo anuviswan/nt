3da5670a7dec75e818706e19da6d90c2ed5a1d33
Anu Viswan <anu.viswan@gmail.com>
2020-10-08 05:46:03 +0530

    Separated Avataar Control

---
+++ nt.desktop/wpf/nt.wpfclient/Nt.Controls/HelperControls/Avataar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nt.Controls.HelperControls
+{
+    /// <summary>
+    /// Interaction logic for Avataar.xaml
+    /// </summary>
+    public partial class Avataar : UserControl
+    {
+        public Avataar()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public string UserName
+        {
+            get { return (string)GetValue(UserNameProperty); }
+            set { SetValue(UserNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UserName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UserNameProperty =
+            DependencyProperty.Register("UserName", typeof(string), typeof(Avataar), new PropertyMetadata("Naina"));
+
+
+    }
+}
