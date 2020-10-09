f0374e5715378c019858b0ceaa91a2b72edc7deb
Anu Viswan <anu.viswan@gmail.com>
2020-10-09 18:32:38 +0530

    Working on Avataar

---
+++ nt.desktop/wpf/nt.wpfclient/Nt.Controls/HelperControls/AvataarContentTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using Nt.Utils.Helper.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Nt.Controls.HelperControls
+{
+    public class AvataarContentTemplateSelector : DataTemplateSelector 
+    {
+        public DataTemplate ImageOnLeftTemplate { get; set; }
+        public DataTemplate ImageOnRightTemplate { get; set; }
+        public DataTemplate ImageOnTopTemplate { get; set; }
+        public DataTemplate ImageOnBottomTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is null) return base.SelectTemplate(item, container);
+            switch ((Direction)item)
+            {
+                case Direction.Left: return ImageOnLeftTemplate;
+                case Direction.Right: return ImageOnRightTemplate;
+                default: return base.SelectTemplate(item, container);
+            }
+        }
+    }
+}
