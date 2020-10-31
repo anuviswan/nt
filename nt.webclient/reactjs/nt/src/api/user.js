e5947da4bfaeba20e316cf2b400ecd10790bc719
Anu Viswan <anu.viswan@gmail.com>
2020-10-31 09:37:21 +0530

    adding interceptor

---
+++ nt.webclient/reactjs/nt/src/api/user.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+// Validate User
+const validateUser = async (userName, passKey) => {
+  const userDetails = {
+    userName: userName,
+    passKey: btoa(passKey),
+  };
+
+  const response = await axios.post(
+    "https://localhost:44353/api/User/ValidateUser",
+    userDetails
+  );
+  return response;
+};
+
+// Get User Profile
+const getUser = async (userName) => {
+  const params = {
+    params: {
+      userName: userName,
+    },
+  };
+
+  var response = await axios.get(
+    "https://localhost:44353/api/User/GetUser",
+    params
+  );
+  return response;
+};
+
+// Update Profile
+const updateUserProfile = async (user) => {
+  const userDetails = {
+    displayName: user.displayName,
+    bio: user.bio,
+  };
+
+  var response = await axios.post(
+    "https://localhost:44353/api/User/UpdateUser",
+    userDetails
+  );
+  return response;
+};
+
+// Change Password
+const changePassword = async (oldPassword, newPassword) => {
+  var recordToUpdate = {
+    oldPassword: oldPassword,
+    newPassword: newPassword,
+  };
+
+  var response = await axios.post(
+    "https://localhost:44353/api/User/ChangePassword",
+    recordToUpdate
+  );
+  return response;
+};
+
+export { changePassword, validateUser, getUser, updateUserProfile };
--- nt.webclient/reactjs/nt/src/index.js
+++ nt.webclient/reactjs/nt/src/index.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
+
+axios.interceptors.request.use((request) => {
+  console.log(request);
+  return request;
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return {
+      data: response.data,
+      hasError: false,
+      error: [],
+    };
+  },
+  (error) => {
+    switch (error.response.status) {
+      case 400:
+        return {
+          data: null,
+          hasError: true,
+          error: [error.response.data],
+        };
+
+      case 401:
+        console.log("okay, in 401 case");
+        alert("Unauthorized access, please login.");
+        this.props.history.push("/");
+        break;
+      case 500:
+        // Handle 500 here
+        break;
+      // and so on..
+      default:
+        break;
+    }
+    // return Promise.reject(error);
+    return {
+      data: null,
+      hasError: true,
+      error: [error.response.data],
+    };
+  }
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
