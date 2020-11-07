f8c42721d63dac8f593ab720015d3a9e1d0251c5
Anu Viswan <anu.viswan@gmail.com>
2020-11-07 11:01:39 +0530

    movie mocks to mock objects

--- nt.webclient/vuejs/nt/src/pages/private/Dashboard.vue
+++ nt.webclient/vuejs/nt/src/pages/private/Dashboard.vue
@@ -14,6 +14,7 @@
 
 <script>
 import ReviewMiniCard from "../../components/review/ReviewMiniCard";
+import { getAllReviews } from "../../api/reviews";
 export default {
   name: "Dashboard",
   components: {
@@ -21,86 +22,13 @@ export default {
   },
   data() {
     return {
-      reviewCollection: [
-        {
-          review: {
-            reviewTitle: "Awesome action !!",
-            movieTitle: "Matrix",
-            review:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
-            upvotes: 3,
-            downvotes: 6,
-          },
-          user: {
-            userName: "Anu Viswan",
-            rating: 5,
-            followers: 345,
-          },
-        },
-        {
-          review: {
-            reviewTitle: "Average action !!",
-            movieTitle: "Matrix",
-            review:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
-            upvotes: 3,
-            downvotes: 6,
-          },
-          user: {
-            userName: "Manu Viswan",
-            rating: 5,
-            followers: 345,
-          },
-        },
-        {
-          review: {
-            reviewTitle: "Super",
-            movieTitle: "Lion King",
-            review:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
-            upvotes: 3,
-            downvotes: 6,
-          },
-          user: {
-            userName: "Jia Anu",
-            rating: 5,
-            followers: 345,
-          },
-        },
-        {
-          review: {
-            reviewTitle: "aha....",
-            movieTitle: "Boss Baby",
-            review:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
-            upvotes: 3,
-            downvotes: 6,
-          },
-          user: {
-            userName: "Naina Anu",
-            rating: 5,
-            followers: 345,
-          },
-        },
-        {
-          review: {
-            reviewTitle: "aha....",
-            movieTitle: "Jumbo",
-            review:
-              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type ",
-            upvotes: 3,
-            downvotes: 6,
-          },
-          user: {
-            userName: "Sreena Anu",
-            rating: 5,
-            followers: 345,
-          },
-        },
-      ],
+      reviewCollection: [],
     };
   },
   methods: {},
+  created() {
+    this.reviewCollection = getAllReviews();
+  },
 };
 </script>
 
