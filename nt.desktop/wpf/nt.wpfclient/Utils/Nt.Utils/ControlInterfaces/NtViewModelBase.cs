8eaeb25f2ee341cd2a0eb24ca787f69b5afcf70b
Anu Viswan <anu.viswan@gmail.com>
2020-11-11 05:55:11 +0530

    Working on UI Update issue

--- nt.desktop/wpf/nt.wpfclient/Utils/Nt.Utils/ControlInterfaces/NtViewModelBase.cs
+++ nt.desktop/wpf/nt.wpfclient/Utils/Nt.Utils/ControlInterfaces/NtViewModelBase.cs
@@ -1,29 +1,29 @@
 ﻿using Caliburn.Micro;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nt.Utils.ControlInterfaces
 {
     public class NtViewModelBase : Screen
     {
-        public object Control { get; set; }
-    }
-    public class NtViewModelBase<TControl> : NtViewModelBase where TControl:NtControlBase
-    {
-        public NtViewModelBase()
+        private NtControlBase control;
+
+        public NtControlBase Control
         {
-            TypedControl.PropertyChanged += ControlPropertyChanged;
+            get => control;
+            set
+            {
+                control = value;
+                control.PropertyChanged += ControlPropertyChanged;
+            }
         }
 
-        private void ControlPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        protected virtual void ControlPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             NotifyOfPropertyChange(nameof(e.PropertyName));
         }
-
-        public TControl TypedControl 
+    }
+    public class NtViewModelBase<TControl> : NtViewModelBase where TControl:NtControlBase
+    {
+        public TControl TypedControl
         {
             get => (TControl)Control;
             set => Control = value;
