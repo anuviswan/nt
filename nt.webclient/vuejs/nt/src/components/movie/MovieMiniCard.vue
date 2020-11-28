7d902f739e731edd492a1c460acd85913e742250
Anu Viswan <anu.viswan@gmail.com>
2020-11-28 18:23:42 +0530

    Added MovieMiniCard Added SearchMovie

---
+++ nt.webclient/vuejs/nt/src/components/movie/MovieMiniCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="card">
+      <img
+        class="card-img-top"
+        src="https://cdn3.iconfinder.com/data/icons/avatars-flat/33/woman_9-512.png"
+        alt="Movie Poster"
+      />
+      <div class="card-body">
+        <h4 class="card-title">{{ this.title }}</h4>
+        <p class="card-text">Language : {{ this.language }}</p>
+        <p class="card-text">
+          Released On : {{ getFormattedDate(new Date(this.releaseDate)) }}
+        </p>
+        <p class="card-text">Ratings : {{ this.ratings }}</p>
+        <a href="#" class="btn btn-primary">See Reviews</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieMiniCard",
+  props: {
+    movie: {
+      required: true,
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      title: "Default Title",
+      language: "Default Language",
+      releaseDate: "20/08/2020",
+      ratings: 4.6,
+    };
+  },
+  created() {
+    this.title = this.movie.title;
+    this.language = this.movie.language;
+    this.releaseDate = this.movie.releaseDate;
+
+    console.log(this.title);
+    console.log(this.language);
+  },
+  methods: {
+    getFormattedDate(date) {
+      let year = date.getFullYear();
+      let month = (1 + date.getMonth()).toString().padStart(2, "0");
+      let day = date
+        .getDate()
+        .toString()
+        .padStart(2, "0");
+
+      return month + "/" + day + "/" + year;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  width: 300px;
+}
+</style>
--- nt.webclient/vuejs/nt/src/pages/private/movie/SearchMovie.vue
+++ nt.webclient/vuejs/nt/src/pages/private/movie/SearchMovie.vue
@@ -3,18 +3,26 @@
     <div>
       <SearchBar v-on:searched="onSearch" promptString="Search Movies..." />
     </div>
-    <div v-if="movieList.length">
-      <UserList v-bind:userList="movieList" />
+    <div class="row">
+      <div
+        v-for="(movie, index) in movieList"
+        :key="index"
+        class="col-md-3 col-6 pt-4"
+      >
+        <MovieMiniCard v-bind:movie="movie" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import SearchBar from "../../../components/generic/SearchBar";
+import MovieMiniCard from "../../../components/movie/MovieMiniCard";
 import { searchMovieByTitle } from "../../../api/movies";
+
 export default {
   name: "SearchMovie",
-  components: { SearchBar },
+  components: { SearchBar, MovieMiniCard },
   data() {
     return {
       movieList: [],
@@ -24,7 +32,13 @@ export default {
     async onSearch(key) {
       console.log(key);
       var response = await searchMovieByTitle(key, -1);
-      console.log(response);
+      if (response.hasError) {
+        this.hasServerError = true;
+        this.serverMessage = response.error;
+        return;
+      }
+
+      this.movieList = response.data;
     },
   },
 };
