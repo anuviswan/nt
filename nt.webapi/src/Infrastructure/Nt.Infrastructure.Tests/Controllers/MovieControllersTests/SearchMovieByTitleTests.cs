826ff673f8c3979054b666cbfe8fef226a2de4c4
Anu Viswan <anu.viswan@gmail.com>
2020-11-28 12:48:53 +0530

    Added 400 Response Test Cases for MovieController.SearchMovieByTitle

--- nt.webapi/src/Infrastructure/Nt.Infrastructure.Tests/Controllers/MovieControllersTests/SearchMovieByTitleTests.cs
+++ nt.webapi/src/Infrastructure/Nt.Infrastructure.Tests/Controllers/MovieControllersTests/SearchMovieByTitleTests.cs
@@ -76,5 +76,45 @@ namespace Nt.Infrastructure.Tests.Controllers.MovieControllersTests
             }
         };
         #endregion
+
+        #region Http Response Code 400
+
+        [Theory]
+        [MemberData(nameof(SearchMovieByTitle_400_TestData))]
+        public async Task SearchMovieByTitle_400(SearchMovieByTitleRequest request)
+        {
+            // Arrange
+            var mockMovieService = new Mock<IMovieService>();
+            mockMovieService.Setup(x => x.SearchMovie(It.IsAny<string>(), It.IsAny<int>()))
+                .Returns(Task.FromResult(EntityCollection.Where(x => x.Title.Contains(request.SearchString, StringComparison.OrdinalIgnoreCase))));
+
+            // Act
+            var movieController = new MovieController(Mapper, mockMovieService.Object);
+            MockModelState(request, movieController);
+            var response = await movieController.SearchMovieByTitle(request);
+
+            // Assert
+            var badRequestObject = Assert.IsType<BadRequestObjectResult>(response.Result);
+            Assert.IsType<SerializableError>(badRequestObject.Value);
+        }
+
+        public static IEnumerable<object[]> SearchMovieByTitle_400_TestData => new List<object[]>
+        {
+            new object[]
+            {
+                new SearchMovieByTitleRequest{SearchString = "t"}
+            },
+            new object[]
+            {
+                new SearchMovieByTitleRequest{SearchString = string.Empty },
+            },
+            new object[]
+            {
+                new SearchMovieByTitleRequest{SearchString = null },
+            }
+        };
+        #endregion
+
+
     }
 }
