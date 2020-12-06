a3602e8245b05665bf7e905f3b134dd50235577b
anu.viswan@gmail.com <njunni@1320>
2020-12-06 16:11:38 +0530

    implemented logout.

--- nt.webclient/vuejs/nt/src/store/modules/user.js
+++ nt.webclient/vuejs/nt/src/store/modules/user.js
@@ -10,6 +10,9 @@ const actions = {
   updateCurrentUser({ commit }, user) {
     commit("updateCurrentUser", user);
   },
+  clearUser({commit}){
+    commit("updateCurrentUser",{})
+  },
   isAuthenticated() {
     if (state.currentUser.token) return true;
     return false;
