4af1a7c5b9512e9ab48dfa64af262f6c9a329a24
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-02-01 05:57:18 +0530

    working on login template

--- nt.webclient/typescriptvuejs/nt/src/components/public/Login.vue
+++ nt.webclient/typescriptvuejs/nt/src/components/public/Login.vue
@@ -1,9 +1,100 @@
 <template>
-  <div>Login Page</div>
+  <div class="card card-block rounded shadow shadow-sm">
+    <div class="card-header bg-primary text-light text-uppercase">
+      <div class="card-title align-middle">
+        <h5 class="mb-0">Sign In</h5>
+      </div>
+    </div>
+    <div class="card-body">
+      <form class="form needs-validation" @submit="onSubmit">
+        <div class="form-group">
+          <input
+            type="text"
+            v-model="user.userName"
+            v-bind:class="
+              hasError('userName')
+                ? 'form-control block is-invalid'
+                : 'form-control block'
+            "
+            placeholder="Username"
+          />
+        </div>
+        <div
+          v-bind:class="
+            hasError('userName') ? 'text-danger text-left' : 'd-none'
+          "
+        >
+          <small>Username cannot be empty</small>
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            v-model="user.password"
+            v-bind:class="
+              hasError('password')
+                ? 'form-control block is-invalid'
+                : 'form-control block'
+            "
+            placeholder="Password"
+          />
+        </div>
+
+        <div
+          v-bind:class="
+            hasError('password') ? 'text-danger text-left' : 'd-none'
+          "
+        >
+          <small>Password cannot be empty</small>
+        </div>
+
+        <div class="form-group">
+          <input
+            type="submit"
+            class="btn btn-block btn-primary"
+            value="Submit"
+          />
+        </div>
+        <div class="d-flex justify-content-center">
+          <ValidationMessage
+            v-bind:messages="serverMessage"
+            v-bind:isError="hasServerError"
+          />
+        </div>
+      </form>
+      <div>
+        Not a member ?
+        <router-link to="/register">Sign up here</router-link>
+      </div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
+import { UserForValidation } from "../../types/user";
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  private user: UserForValidation;
+  private serverMessage: string;
+
+  constructor() {
+    super();
+
+    this.serverMessage = "";
+    this.user = {
+      userName: "",
+      password: "",
+    };
+  }
+
+  hasServerError(): boolean {
+    return false;
+  }
+  hasError(key: string): boolean {
+    return true;
+  }
+
+  onSubmit(): void {
+    console.log("form submitted");
+  }
+}
 </script>
---
+++ nt.webclient/typescriptvuejs/nt/src/types/user.ts
@@ -0,0 +1,4 @@
+export interface UserForValidation{
+    userName:string,
+    password:string,
+}
