42d03addbb2239ef4469812b00c073acca2262ed
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-02-05 04:22:29 +0530

    Working on user store

--- nt.webclient/typescriptvuejs/nt/src/store/module/user.ts
+++ nt.webclient/typescriptvuejs/nt/src/store/module/user.ts
@@ -1,8 +1,22 @@
 import {VuexModule, Module, Mutation, Action} from "vuex-module-decorators"
-
+import {User} from "../../types/user";
 @Module
 
-export default class User extends VuexModule{
+export default class UserStore extends VuexModule{
+    public currentUser:User ={
+        UserName : '',
+        DisplayName:''
+    }        
 
-    
+    @Mutation
+    public setCurrentUser(newUser:User):void{
+        this.currentUser = newUser;
+    }
+
+    @Action
+    public updateCurrentUser(newUser:User):void{
+        this.context.commit('setCurrentUser',newUser);
+    } 
+
 }
+
