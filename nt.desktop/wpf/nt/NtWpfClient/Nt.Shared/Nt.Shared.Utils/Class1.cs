3994d66346073aa1df494e53b20d56f3bc8efbab
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-03-05 04:53:56 +0530

    Adding shared library

---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Shared/Nt.Shared.Utils/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nt.Shared.Utils
+{
+    public class Class1
+    {
+    }
+}
---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Shared/Nt.Shared.Utils/Helpers/PropertyChangedBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Nt.Shared.Utils.Helpers
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyOnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
