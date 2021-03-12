1892799ab0743656504cbb7019dfcc56944015c9
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-03-13 03:34:40 +0530

    Working on base classes

---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Controls/Nt.Controls.HelperControls/WindowManager/WindowManagerControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nt.Shared.Utils.ControlBase;
+
+namespace Nt.Controls.HelperControls.WindowManager
+{
+    public class WindowManagerControl:ControlBase
+    {
+        public ViewModelBase ChildViewModel { get; set; }
+    }
+}
---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Shared/Nt.Shared.Utils/ControlBase/ControlBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nt.Shared.Utils.Helpers;
+
+namespace Nt.Shared.Utils.ControlBase
+{
+    public abstract class ControlBase:PropertyChangedBase
+    {
+
+    }
+
+
+    public abstract class ControlBase<TViewModel>:ControlBase where TViewModel : ViewModelBase
+    {
+
+    }
+}
