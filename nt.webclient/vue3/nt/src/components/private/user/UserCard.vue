4795fbaa47a4079ff6fa7dc6bef60593555cd577
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-04-05 05:11:37 +0530

    adding ViewProfile and User Card

---
+++ nt.webclient/vue3/nt/src/components/private/user/UserCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="card border-primary mb-3">
+    <div class="card-avataar py-2">
+      <img
+        class=" rounded-circle img-thumbnail"
+        src="https://cdn3.iconfinder.com/data/icons/avatars-flat/33/woman_9-512.png"
+        alt="Avataar Pic"
+        width="100"
+      />
+    </div>
+
+    <div class="card-body mx-auto">
+      <h4 class="card-title block text-uppercase text-center">
+        {{ selectedUser.value.displayName }}
+      </h4>
+      {{ selectedUser.value.userName }}
+      <!-- <Rating value="{5}" totalStars="{5}" /> -->
+    </div>
+
+    <div class="card-header">
+      <div class="row">
+        <div class="col-lg-4 card-metadata text-center">45</div>
+        <div class="col-lg-4 card-metadata text-center">345</div>
+        <div class="col-lg-4 card-metadata text-center">1.1K</div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 card-metadata-footer text-center">Reviews</div>
+        <div class="col-lg-4 card-metadata-footer text-center">Followed</div>
+        <div class="col-lg-4 card-metadata-footer text-center">Followers</div>
+      </div>
+    </div>
+    <div class="card-body mx-auto">{{ selectedUser.value.bio }}</div>
+  </div>
+</template>
+<script>
+import { toRefs } from "vue";
+export default {
+  name: "UserCard",
+  props: {
+    user: {
+      required: true,
+      type: Object,
+    },
+  },
+  setup(props) {
+    console.log(props.user);
+    const { selectedUser } = toRefs(props);
+    console.log("Loading User Card");
+    console.log(selectedUser.value.user);
+    // let selectedUser = reactive({});
+    // watch(() => {
+    //   const { selectedUser } = props.user;
+    // });
+    // console.log("Loading User Card : " + props.user.userName);
+    // console.log(props.user);
+    // userName.value = props.user.userName;
+    // displayName = props.user.displayName;
+    // bio = props.user.bio;
+    // rating = props.user.rating;
+
+    // return () => {
+    //   const { user } = props.user;
+    //   console.log("Loading User Card");
+    //   console.log(props.user);
+    //   return user;
+    // };
+
+    return {
+      selectedUser,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  max-width: 350px;
+}
+</style>
