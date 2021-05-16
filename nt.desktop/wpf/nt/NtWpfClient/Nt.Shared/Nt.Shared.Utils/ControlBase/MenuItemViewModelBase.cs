64ae9c43ab590735bdd611677d920f6294489055
anu.viswan@gmail.com <anu.viswan@gmail.com>
2021-05-16 05:40:26 +0530

    Working on Menu base

---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Shared/Nt.Shared.Utils/ControlBase/MenuItemViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using Nt.Shared.Utils.Interfaces;
+
+namespace Nt.Shared.Utils.ControlBase
+{
+    public class MenuItemViewModelBase : ViewModelBase, IHasMenuReference
+    {
+        public virtual string Name { get; set; }
+        public virtual object Icon { get; set; }
+        public virtual ViewModelBase ViewModel { get; }
+    }
+}
---
+++ nt.desktop/wpf/nt/NtWpfClient/Nt.Shared/Nt.Shared.Utils/Interfaces/IHasMenuReference.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nt.Shared.Utils.Interfaces
+{
+    class IHasMenuReference
+    {
+    }
+}
