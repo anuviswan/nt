7c7bd0eb422580602428e835dbbacebc74e297ef
Anu Viswan <anu.viswan@gmail.com>
2021-12-14 21:33:14 +0530

    Setting up User controller

---
+++ server/nt.microservice/services/UserService/UserService.Api/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Api.Controllers;
+public class UserController : Controller
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+    public UserController(IMediator mediator,IMapper mapper)
+    {
+        (_mediator,_mapper) = (mediator,mapper);
+    }
+
+}
--- server/nt.microservice/services/UserService/UserService.Api/GlobalUsings.cs
+++ server/nt.microservice/services/UserService/UserService.Api/GlobalUsings.cs
@@ -2,3 +2,5 @@
 global using UserService.Data.Database;
 global using UserService.Data.Repository;
 global using UserService.Service.Command;
+global using Microsoft.AspNetCore.Mvc;
+global using AutoMapper; 
