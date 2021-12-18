7aaf52a4dcf050cfbd834d60ef5ca79a456bc3f5
Anu Viswan <anu.viswan@gmail.com>
2021-12-18 15:39:01 +0530

    code cleanup

--- server/nt.microservice/services/UserService/UserService.Api.Tests/ControllerTests/UserControllerTests/RegisterUserTests.cs
+++ server/nt.microservice/services/UserService/UserService.Api.Tests/ControllerTests/UserControllerTests/RegisterUserTests.cs
@@ -1,13 +1,4 @@
-﻿using AutoMapper;
-using FluentAssertions;
-using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
-using System.Threading;
-using System.Threading.Tasks;
-using UserService.Api.Controllers;
-using UserService.Api.ViewModels.User;
-using UserService.Service.Command;
+﻿
 
 namespace UserService.Api.Tests.ControllerTests.UserControllerTests;
 
@@ -74,7 +65,6 @@ public class RegisterUserTests:ControllerTestBase
     [MemberData(nameof(RegisterUser_ValidData_ShouldFail_TestData))]
     public async Task RegisterUser_ValidData_ShouldFail(CreateUserRequestViewModel request, SerializableError expectedError)
     {
-        var cancellationToken = default(CancellationToken);
         var mockMediator = new Moq.Mock<IMediator>();
         mockMediator.Setup(x => x.Send(It.IsAny<CreateUserCommand>(), It.IsAny<CancellationToken>()))
             .Returns<CreateUserCommand, CancellationToken>((x, _) => Task.FromResult(new UserMetaInformation
--- server/nt.microservice/services/UserService/UserService.Api.Tests/GlobalUsings.cs
+++ server/nt.microservice/services/UserService/UserService.Api.Tests/GlobalUsings.cs
@@ -1,3 +1,13 @@
 global using Xunit;
 global using UserService.Domain.Entities;
 global using System.Collections.Generic;
+global using AutoMapper;
+global using FluentAssertions;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+global using Moq;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using UserService.Api.Controllers;
+global using UserService.Api.ViewModels.User;
+global using UserService.Service.Command;
