8a952c9c88c7d12c85390315f1dfe647f55e1d9b
Anu Viswan <anu.viswan@gmail.com>
2021-12-20 22:16:08 +0530

    Add CQRS handler for ValidateUser Query

---
+++ server/nt.microservice/AuthService.Service/Query/ValidateUserQuery.cs
@@ -0,0 +1,8 @@
+﻿using AuthService.Domain.Entities;
+using MediatR;
+
+namespace AuthService.Service.Query;
+public class ValidateUserQuery:IRequest<bool>
+{
+    public User User { get; set; }
+}
---
+++ server/nt.microservice/AuthService.Service/Query/ValidateUserQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using AuthService.Data.Repository;
+using MediatR;
+
+namespace AuthService.Service.Query;
+public class ValidateUserQueryHandler : IRequestHandler<ValidateUserQuery, bool>
+{
+    private readonly IUserRepository _userRepository;
+    public ValidateUserQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    public async Task<bool> Handle(ValidateUserQuery request, CancellationToken cancellationToken)
+    {
+        return await _userRepository.ValidateUser(request.User);
+    }
+}
