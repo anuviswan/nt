4ad8998a1ca514fd5eaed14ee80d6bdf0f73b821
Anu Viswan <anu.viswan@gmail.com>
2022-04-08 21:26:39 +0530

    Configururing logs

--- server/nt.microservice/services/UserService/UserService.Api/Controllers/UserController.cs
+++ server/nt.microservice/services/UserService/UserService.Api/Controllers/UserController.cs
@@ -10,7 +10,7 @@ public class UserController : Controller
 {
     private readonly IMediator _mediator;
     private readonly IMapper _mapper;
-    public UserController(IMediator mediator,IMapper mapper)
+    public UserController(IMediator mediator,IMapper mapper, ILogger logger)
     {
         (_mediator,_mapper) = (mediator,mapper);
     }
