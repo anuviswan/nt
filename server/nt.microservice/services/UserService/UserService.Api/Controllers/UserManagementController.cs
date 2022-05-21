f2a7cb6ec1eeafe3e0e7539076bc179ca68836b0
Anu Viswan <anu.viswan@gmail.com>
2022-05-21 20:50:59 +0530

    Adding VMs

---
+++ server/nt.microservice/services/UserService/UserService.Api/Controllers/UserManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Api.ViewModels.UserManagement;
+
+namespace UserService.Api.Controllers;
+
+[ApiController]
+[Route("Users")]
+public class UserManagementController : Controller
+{
+    public Action<IEnumerable<SearchUserResponseViewModel>> SearchUser(SearchUserRequestViewModel searchTerms)
+    {
+        return default;
+    }
+}
---
+++ server/nt.microservice/services/UserService/UserService.Api/ViewModels/UserManagement/SearchUserRequestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Api.ViewModels.UserManagement;
+
+public record SearchUserRequestViewModel
+{
+    [Required(ErrorMessage = $"{nameof(SearchPart)} is mandatory.")]
+    [MinLength(3, ErrorMessage = $"{nameof(SearchPart)} should be of minimum 3 characters")]
+    public string SearchPart { get; set; }
+}
