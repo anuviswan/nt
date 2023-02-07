f5fa4fd52c2664f9b950d9d4bc615ec4dd5b77b3
Anu Viswan <anu.viswan@gmail.com>
2023-02-07 20:12:00 +0530

    Adding seeding data

---
+++ server/nt.microservice/services/AuthService/AuthService.Data/Migrations/InitialSeed202302072003.cs
@@ -0,0 +1,28 @@
+﻿using AuthService.Domain.Entities;
+using FluentMigrator;
+
+namespace AuthService.Data.Migrations;
+
+[Migration(202302072003)]
+public class InitialSeed202302072003 : Migration
+{
+    public override void Down()
+    {
+        Delete.FromTable(User.TABLE_NAME)
+            .AllRows();
+        Delete.Table(User.TABLE_NAME);
+    }
+
+    public override void Up()
+    {
+        Create.Table(User.TABLE_NAME)
+            .WithColumn("id").AsGuid().NotNullable().Identity().PrimaryKey()
+            .WithColumn("username").AsString().NotNullable()
+            .WithColumn("password").AsString().NotNullable();
+
+
+        Insert.IntoTable(User.TABLE_NAME)
+            .Row(new { username = "jiaanu", password = "bXlwYXNz" })
+            .Row(new { username = "nainaanu", password = "bXlwYXNz" });
+    }
+}
