820122ee7fcbfd28ea0a3303ede9fef6ae764414
Anu Viswan <anu.viswan@gmail.com>
2023-02-07 21:30:36 +0530

    implemented fluent migrations

---
+++ server/nt.microservice/services/AuthService/AuthService.Api/Helpers/ExtensionMethods/IApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator.Runner;
+
+namespace AuthService.Api.Helpers.ExtensionMethods;
+
+public static class MigrationsExtensions
+{
+    public static IApplicationBuilder Migrate(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var migrator = scope.ServiceProvider.GetService<IMigrationRunner>();
+        migrator?.ListMigrations();
+        migrator?.MigrateUp();
+        return app;
+    }
+}
