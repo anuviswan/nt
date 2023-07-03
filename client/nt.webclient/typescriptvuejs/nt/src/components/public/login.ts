1f7af3439e6bb68eefc745334b78dfd0ed34c619
Anu Viswan <anu.viswan@beo.in>
2023-07-03 16:37:45 +0530

    removed typescript implementation

--- client/nt.webclient/typescriptvuejs/nt/src/components/public/login.ts
+++
@@ -1,7 +0,0 @@
-import {LoginUser} from "../../types/loginuser"
-import {ServerResponse} from "../../types/baseinterfaces"
-export function SubmitForm(user:LoginUser):ServerResponse{
-    
-    return {HasError:false,Message:''};
-
-}
