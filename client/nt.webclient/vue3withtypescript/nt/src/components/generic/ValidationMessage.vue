f599133d7fc30239906b80987f108d3e81e9b3af
Anu Viswan <anu.viswan@gmail.com>
2023-07-06 20:44:40 +0530

    Added ValidationMessage.vue

---
+++ client/nt.webclient/vue3withtypescript/nt/src/components/generic/ValidationMessage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-bind:class="getMessageClass()">
+      <div v-if="messages.length > 1">
+        <ul>
+          <li v-for="(msg, index) in messages" :key="index">
+            <small>{{ msg }}</small>
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <small>{{ messages[0] }}</small>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { PropType, withDefaults, defineProps } from "vue"
+
+  interface Props{
+      messages: string[], 
+      isError: boolean 
+  }
+    const props = withDefaults(defineProps<Props>(),{
+      messages : ()=>[],
+    });
+  
+    const getMessageClass = () => {
+        if (!props.messages) return;
+  
+        if (props.messages.length == 0) {
+          return "d-none";
+        }
+  
+        return props.isError
+          ? "text-danger text-justify"
+          : "text-success text-center";
+      }
+      console.log(props);
+  
+  </script>
+  
+  <style></style>
--- client/nt.webclient/vue3withtypescript/nt/src/components/public/RegisterUser.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/public/RegisterUser.vue
@@ -1,130 +1,103 @@
 <template>
-    <div class="card card-block rounded shadow shadow-sm" >
-      <div class="card-header bg-primary text-light text-uppercase">
-        <div class="card-title align-middle">
-          <h5 class="mb-0">Sign Up</h5>
+    <div class="card card-block rounded shadow shadow-sm">
+        <div class="card-header bg-primary text-light text-uppercase">
+            <div class="card-title align-middle">
+                <h5 class="mb-0">Sign Up</h5>
+            </div>
         </div>
-      </div>
-      <div class="card-body">
-        <form class="form needs-validation" @submit.prevent="onSubmit">
-          <div class="form-group">
-            <input
-              type="text"
-              v-model="formData.userName"
-              v-bind:class="
-                hasError('userName')
-                  ? 'form-control block is-invalid'
-                  : 'form-control block'
-              "
-              placeholder="Username"
-            />
-          </div>
-          <!-- <div class="d-flex justify-content-left" v-if="v$.userName.$error">
-          <ValidationMessage   :messages="v$.userName.$errors.map(x=>x.$message)"
-              v-bind:isError="true"
-            />
-          </div> -->
-          <div class="form-group">
-            <input
-              type="password"
-              v-model="formData.password"
-              v-bind:class="
-                hasError('password')
-                  ? 'form-control block is-invalid'
-                  : 'form-control block'
-              "
-              placeholder="Password"
-            />
-          </div>
-          <!-- <div class="d-flex justify-content-left" v-if="v$.password.$error">
-          <ValidationMessage   :messages="v$.password.$errors.map(x=>x.$message)"
-              v-bind:isError="true"
-            />
-          </div> -->
-          <div class="form-group">
-            <input
-              type="password"
-              v-model="formData.confirmPassword"
-              v-bind:class="
-                hasError('confirmPassword')
-                  ? 'form-control block is-invalid'
-                  : 'form-control block'
-              "
-              placeholder="Confirm Password"
-            />
-          </div>
-          <!-- <div class="d-flex justify-content-left" v-if="v$.confirmPassword.$error">
-          <ValidationMessage   :messages="v$.confirmPassword.$errors.map(x=>x.$message)"
-              v-bind:isError="true"
-            />
-          </div> -->
-  
-          <div class="form-group">
-            <input
-              type="submit"
-              class="btn btn-block btn-primary"
-              value="Submit"
-            />
-          </div>
-        </form>
-        <div>
-          Already a member ?
-          <router-link to="/">Sign in here</router-link>
+        <div class="card-body">
+            <form class="form needs-validation" @submit.prevent="onSubmit">
+                <div class="form-group">
+                    <input type="text" v-model="formData.userName" v-bind:class="hasError('userName')
+                        ? 'form-control block is-invalid'
+                        : 'form-control block'
+                        " placeholder="Username" />
+                </div>
+                <div class="d-flex justify-content-left" v-if="v$.userName.$error">
+                    <ValidationMessage :messages="v$.formData.userName.$errors.map((x:any) => x.$message)" v-bind:isError="true" />
+                </div>
+                <div class="form-group">
+                    <input type="password" v-model="formData.password" v-bind:class="hasError('password')
+                        ? 'form-control block is-invalid'
+                        : 'form-control block'
+                        " placeholder="Password" />
+                </div>
+                <div class="d-flex justify-content-left" v-if="v$.password.$error">
+                    <ValidationMessage :messages="v$.formData.password.$errors.map((x:any) => x.$message)" v-bind:isError="true" />
+                </div>
+                <div class="form-group">
+                    <input type="password" v-model="formData.confirmPassword" v-bind:class="hasError('confirmPassword')
+                        ? 'form-control block is-invalid'
+                        : 'form-control block'
+                        " placeholder="Confirm Password" />
+                </div>
+                <div class="d-flex justify-content-left" v-if="v$.formData.confirmPassword.$error">
+                    <ValidationMessage :messages="v$.confirmPassword.$errors.map((x : any) => x.$message)" v-bind:isError="true" />
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" class="btn btn-block btn-primary" value="Submit" />
+                </div>
+            </form>
+            <div>
+                Already a member ?
+                <router-link to="/">Sign in here</router-link>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
+</template>
   
-  <script setup lang="ts">
-  import { ref, computed } from "vue";
-//   import ValidationMessage from "@/components/generic/ValidationMessage";
-//   import { useVuelidate } from '@vuelidate/core'
-//   import { required, minLength,sameAs, helpers  } from '@vuelidate/validators'
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import ValidationMessage from "@/components/generic/ValidationMessage";
+import { useVuelidate } from '@vuelidate/core'
+import { required, minLength, sameAs, helpers } from '@vuelidate/validators'
 //   import {registerUser} from "@/api/user.js"
 
 
- interface IFormData{
-    userName : string;
-    password : string;
-    confirmPassword : string;
- }
+interface IFormData {
+    userName: string;
+    password: string;
+    confirmPassword: string;
+}
 
- const formData = ref<IFormData>({
-    userName : '',
-    password : '',
-    confirmPassword : ''
- });
+const formData = ref<IFormData>({
+    userName: '',
+    password: '',
+    confirmPassword: ''
+});
+
+const rules = computed(() => ({
+    formData: {
+        userName: {
+            required: helpers.withMessage('Username cannot be empty', required),
+            minLengthValue: helpers.withMessage('Username should minimum 4 characters', minLength(4))
+        },
+        password: { required: helpers.withMessage('Password cannot be empty', required), },
+        confirmPassword: { sameAsPassword: helpers.withMessage('Password do not match', sameAs(formData.value.password)) }
+    }
+
+}))
+
+function hasError(d:any):boolean{
+    console.log(d)
+    return false;
+}
+
+const v$ = useVuelidate(rules, { formData });
+
+const onSubmit = async (): Promise<void> => {
+
+    var validationResult = await v$.value.$validate();
+    if (!validationResult) {
+        console.log("Validation failed");
+        return;
+    }
+    //    await registerUser(userName.value,'',password.value);
+}
+
+
+
+</script>
   
-  
-  
-//   const rules = computed(()=>({
-//     userName : {
-//                   required:helpers.withMessage('Username cannot be empty', required), 
-//                   minLengthValue: helpers.withMessage('Username should minimum 4 characters',minLength(4)) 
-//                },
-//     password : {required:helpers.withMessage('Password cannot be empty', required),  },
-//     confirmPassword : { sameAsPassword: helpers.withMessage('Password do not match', sameAs(password))}
-//   }))
-  
-  const hasError = (d:any)=> {
-    console.log(d);
-    return true;
-  }
-  
-//   const v$ = useVuelidate(rules,{userName,password,confirmPassword});
-  
-  const onSubmit = async () => {
-   
-    // var validationResult = await v$.value.$validate();
-    // if (!validationResult) {
-    //       console.log("Validation failed");
-    //       return;
-    //     }
-    //     await registerUser(userName.value,'',password.value);
-  }
-  
-  
-  
-  </script>
-  
-  <style></style>
+<style></style>
