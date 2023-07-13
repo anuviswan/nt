d28a16de6b3ef15b7027fe89e005d90ab547c93a
Anu Viswan <anu.viswan@gmail.com>
2023-07-13 21:44:52 +0530

    Updated API endpoints to match repo-definitions

--- server/nt.microservice/services/UserService/UserService.Api/ViewModels/User/CreateUserResponseViewModel.cs
+++ server/nt.microservice/services/UserService/UserService.Api/ViewModels/User/CreateUserResponseViewModel.cs
@@ -1,5 +1,11 @@
-﻿namespace UserService.Api.ViewModels.User;
+﻿using System.Text.Json.Serialization;
+
+namespace UserService.Api.ViewModels.User;
 public record CreateUserResponseViewModel
 {
+    [JsonPropertyName("userName")]
     public string UserName { get; init; }
+
+    [JsonPropertyName("displayName")]
+    public string DisplayName { get;init; }
 }
