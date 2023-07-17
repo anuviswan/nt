3b18fae7efbbe4abb3223cc5bcb1590445c2d8d7
Anu Viswan <anu.viswan@gmail.com>
2023-07-17 06:36:18 +0530

    added mongo container

--- client/nt.desktop/wpf/nt.wpfclient/Utils/Nt.Utils/Helper/HttpUtils.cs
+++ client/nt.desktop/wpf/nt.wpfclient/Utils/Nt.Utils/Helper/HttpUtils.cs
@@ -8,7 +8,7 @@ namespace Nt.Utils.Helper;
 
 public static class HttpUtils
 {
-    public static string ServerUrl = "https://localhost:44353";
+    public static string ServerUrl = "https://localhost:27017";
 
     public static string ValidateUserUrl = @"/api/User/ValidateUser";
     public static string UpdateUserUrl = @"/api/User/UpdateUser";
