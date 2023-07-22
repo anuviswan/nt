6ea80737edf4d4bc9b981bf1e735a4cd1a4cbbbd
Anu Viswan <anu.viswan@gmail.com>
2023-07-22 17:41:31 +0530

    adding private container/dashboard

---
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/generic/NavigationBar.vue
@@ -0,0 +1,115 @@
+<template>
+     <nav
+    class="navbar navbar-expand-lg navbar-dark bg-primary navbar-fixed-top py-0"
+  >
+    <a class="navbar-brand" href="#">November Talkies</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" exact to="/p/dashboard"
+            >Home</router-link
+          >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" exact to="/p/UserSearch"
+            >Users</router-link
+          >
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbardrop"
+            data-toggle="dropdown"
+          >
+            Movies
+          </a>
+          <div class="dropdown-menu">
+            <router-link
+              class="dropdown-item"
+              exact
+              v-bind:to="`/p/movie/search`"
+            >
+              Search
+            </router-link>
+            <router-link
+              class="dropdown-item"
+              exact
+              v-bind:to="`/p/movie/create`"
+            >
+              Create
+            </router-link>
+          </div>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown active">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbardrop"
+            data-toggle="dropdown"
+          >
+            {{ currentUser.userName }}
+          </a>
+          <div class="dropdown-menu dropdown-menu-right">
+            <router-link
+              class="dropdown-item"
+              exact
+              v-bind:to="`/p/user/${currentUser.userName}`"
+            >
+              View Profile
+            </router-link>
+            <router-link
+              exact
+              v-bind:to="`/p/user/${currentUser.userName}/edit`"
+              class="dropdown-item"
+            >
+              Edit User
+            </router-link>
+            <router-link
+              class="dropdown-item"
+              exact
+              v-bind:to="`/p/user/${currentUser.userName}/chpwd`"
+              >Change Password</router-link
+            >
+            <div class="dropdown-divider"></div>
+            <label class="dropdown-item" href="#" v-on:click="logout"
+              >Log out</label
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue"
+import {useUserStore} from "@/stores/userStore"
+
+const store = useUserStore();
+const currentUser = ref({
+  userName: store.UserName,
+  displayName: store.DisplayName,
+  bio: store.Bio
+});
+console.log(currentUser.value.userName);
+console.log("getter");
+</script>
+
+<style scoped>
+</style>
+
---
+++ client/nt.webclient/vue3withtypescript/nt/src/pages/private/PrivateContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <NavigationBar />
+      <div class="pt-5">
+        <router-view></router-view>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import NavigationBar from "@/components/private/generic/NavigationBar";
+  </script>
+  
+  <style></style>
