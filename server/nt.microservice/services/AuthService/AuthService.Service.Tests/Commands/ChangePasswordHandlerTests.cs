02bf7bb304211365f985c8eca29e410a45f05b50
Anu Viswan <anu.viswan@gmail.com>
2023-08-22 21:50:14 +0530

    Updating test case

--- server/nt.microservice/services/AuthService/AuthService.Service.Tests/Commands/ChangePasswordHandlerTests.cs
+++ server/nt.microservice/services/AuthService/AuthService.Service.Tests/Commands/ChangePasswordHandlerTests.cs
@@ -8,10 +8,16 @@ namespace AuthService.Service.Tests.Commands;
 [TestFixture]
 public class ChangePasswordHandlerTests
 {
+    private IUnitOfWorkFactory _unitOfWorkFactory = null!;
+    private IUnitOfWork _unitOfWork = null!;
+    private IUserRepository _userRepository = null!;
+
     [SetUp]
     public void Initialize()
     {
-
+        var _unitOfWorkFactory = Substitute.For<IUnitOfWorkFactory>();
+        var _unitOfWork = Substitute.For<IUnitOfWork>();
+        var _userRepository = Substitute.For<IUserRepository>();
     }
 
     [Test]
@@ -19,31 +25,26 @@ public class ChangePasswordHandlerTests
     public async Task ChangePasswordHandler_Handle_ValidData_Success(ChangePasswordCommand command)
     {
         #region Arrange
-        var uowFactory = Substitute.For<IUnitOfWorkFactory>();
-        var uow = Substitute.For<IUnitOfWork>();
-        var userRepository = Substitute.For<IUserRepository>();
-        
-        uowFactory.CreateUnitOfWork().Returns(_ => uow);
-        uow.UserRepository.Returns(_ => userRepository);
-        
-        userRepository.ValidateUserAsync(Arg.Any<Domain.Entities.User>()).Returns(_ => new User
+        _unitOfWorkFactory.CreateUnitOfWork().Returns(_ => _unitOfWork);
+        _unitOfWork.UserRepository.Returns(_ => _userRepository);
+
+        _userRepository.ValidateUserAsync(Arg.Any<Domain.Entities.User>()).Returns(_ => new User
         {
             UserName = "Test",
             Password = "Test",
         });
-        userRepository.ChangePasswordAsync(Arg.Any<User>(), Arg.Any<string>()).Returns(_ => true);
+        _userRepository.ChangePasswordAsync(Arg.Any<User>(), Arg.Any<string>()).Returns(_ => true);
         #endregion
 
 
         #region Act
-        var sut = new ChangePasswordCommandHandler(uowFactory);
+        var sut = new ChangePasswordCommandHandler(_unitOfWorkFactory);
         var result = await sut.Handle(command, default).ConfigureAwait(false);
         #endregion
 
         #region Assert
         Assert.IsTrue(result);
         #endregion
-
     }
 
     static IEnumerable<object[]> ChangePasswordHandler_Handle_ValidData_Success_TestData()
@@ -54,7 +55,7 @@ public class ChangePasswordHandlerTests
             {
                 NewPassword = "Test1",
                 OldPassword = "Test",
-                UserName = "ABEC"
+                UserName = "Test"
             }
         };
     }
