c8548a06175c954c1b597829460bb4e333da0edb
Anu Viswan <anu.viswan@gmail.com>
2023-08-22 21:25:12 +0530

    Creating unit test for AuthService

---
+++ server/nt.microservice/services/AuthService/AuthService.Service.Tests/Commands/ChangePasswordHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.Service.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Service.Tests.Commands;
+
+[TestFixture]
+public class ChangePasswordHandlerTests
+{
+    [SetUp]
+    public void Initialize()
+    {
+
+    }
+
+
+    public async Task ChangePasswordHandler_Handle_ValidData_Success(ChangePasswordCommand command)
+    {
+        
+    }
+}
---
+++ server/nt.microservice/services/AuthService/AuthService.Service.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AuthService.Service.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
