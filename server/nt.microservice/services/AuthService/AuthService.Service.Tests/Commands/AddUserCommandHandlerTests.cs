d264007921963719606dfa010c01bee14c5b35bb
Anu Viswan <anu.viswan@beo.in>
2023-08-23 09:32:45 +0530

    adding more test

--- server/nt.microservice/services/AuthService/AuthService.Service.Tests/Commands/AddUserCommandHandlerTests.cs
+++ server/nt.microservice/services/AuthService/AuthService.Service.Tests/Commands/AddUserCommandHandlerTests.cs
@@ -1,10 +1,8 @@
 ﻿using AuthService.Data.Database;
 using AuthService.Data.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using AuthService.Domain.Entities;
+using AuthService.Service.Command;
+using AuthService.Service.Helpers.Exceptions;
 
 namespace AuthService.Service.Tests.Commands;
 
@@ -21,21 +19,86 @@ public class AddUserCommandHandlerTests
         _unitOfWorkFactory = Substitute.For<IUnitOfWorkFactory>();
         _unitOfWork = Substitute.For<IUnitOfWork>();
         _userRepository = Substitute.For<IUserRepository>();
+
+        _unitOfWorkFactory.CreateUnitOfWork().Returns(_ => _unitOfWork);
+        _unitOfWork.UserRepository.Returns(_ => _userRepository);
     }
 
     [Test]
-    public void AddUser_Handle_ValidData_Success()
+    [TestCaseSource(nameof(AddUser_Handle_ValidData_Success_TestData))]
+    public async Task AddUser_Handle_ValidData_Success(AddUserCommand request,User expectedResponse)
     {
         #region Arrange
-        _unitOfWorkFactory.CreateUnitOfWork().Returns(_ => _unitOfWork);
-        _unitOfWork.UserRepository.Returns(_ => _userRepository);
+        _userRepository.GetAll().Returns(x=> Enumerable.Empty<User>());
+        _userRepository.AddAsync(Arg.Any<User>()).Returns(x=> expectedResponse);
         #endregion
 
         #region Act
-
+        var sut = new AddUserCommandHandler(_unitOfWorkFactory);
+        var response = await sut.Handle(request, default).ConfigureAwait(false);
         #endregion
 
         #region Assert
+        Assert.IsNotNull(response);
+        Assert.That(response, Is.EqualTo(expectedResponse));
         #endregion
+    }
+
+    static IEnumerable<object[]> AddUser_Handle_ValidData_Success_TestData()
+    {
+        yield return new object[]
+        {
+            new AddUserCommand
+            {
+                User = new User
+                {
+                    UserName = "John Doe",
+                    Password = "Random"
+                },
+
+            },
+            new User
+            {
+                UserName = "John Doe",
+                Password = "Random"
+            }
+        };
+    }
+
+
+    [Test]
+    [TestCaseSource(nameof(AddUser_Handle_UserExists_Success_TestData))]
+    public void AddUser_Handle_UserExists_Success(AddUserCommand request, User expectedResponse)
+    {
+        #region Arrange
+        _userRepository.GetAll().Returns(x => new[] { expectedResponse });
+        _userRepository.AddAsync(Arg.Any<User>()).Returns(x => expectedResponse);
+        #endregion
+
+        #region Act
+        var sut = new AddUserCommandHandler(_unitOfWorkFactory);
+        Assert.ThrowsAsync<UserAlreadyExistsException>(async () => await sut.Handle(request, default).ConfigureAwait(false));
+        #endregion
+    }
+
+    static IEnumerable<object[]> AddUser_Handle_UserExists_Success_TestData()
+    {
+        yield return new object[]
+        {
+            new AddUserCommand
+            {
+                User = new User
+                {
+                    UserName = "John Doe",
+                    Password = "Random"
+                },
+
+            },
+            new User
+            {
+                UserName = "John Doe",
+                Password = "Random"
+            }
+        };
     }
 }
