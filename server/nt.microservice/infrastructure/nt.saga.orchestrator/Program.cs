2b47c151e35198584182b193a764171e869b6909
Anu Viswan <anu.viswan@gmail.com>
2023-09-26 21:31:56 +0530

    Working on loadbalancer - resolving upstream servers

--- server/nt.microservice/infrastructure/nt.saga.orchestrator/Program.cs
+++ server/nt.microservice/infrastructure/nt.saga.orchestrator/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<AuthService>(options =>
 {
-    options.BaseAddress = new Uri("http://authservice.com/");
+    options.BaseAddress = new Uri("http://host.docker.internal:8100/");
 });
 
 builder.Services.AddHttpClient<UserService>(options =>
