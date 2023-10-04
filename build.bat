f6519faeadc12da4c1387b677ff656212cb1b991
Anu Viswan <anu.viswan@gmail.com>
2023-10-04 20:38:15 +0530

    scale with docker compose use expose instead of port use service name in nginx.conf

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 docker-compose -f client\nt.webclient\vue3\nt\docker-compose.yml up -d
-docker-compose -f server/nt.microservice/docker-compose.yml -f server/nt.microservice/docker-compose.override.yml up --scale nt.authservice.service=5 -d
+docker-compose -f server\nt.microservice\docker-compose.yml -f server\nt.microservice\docker-compose.override.yml up --scale nt.authservice.service=5 -d
