ffbd0790e07ce999b5b8d3875ee081f48df45ef3
Anu Viswan <a.viswan@kruss.de>
2024-05-06 14:37:00 +0530

    Updated user profile ui

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/user/UserProfileCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/user/UserProfileCard.vue
@@ -1,16 +1,46 @@
 <template>
     <div class="container-fluid">
       <div class="row align-items-center justify-content-center">
-        <div class="col-sm-8 col-md-6 col-lg-6">
+
+        <!-- Mini Card -->
+        <div class="col-sm-12 col-md-6 col-lg-4" v-if="isMiniCard" width="400">
+          <div>
           <div class="card card-block rounded shadow shadow-sm">
             <div class="card-header bg-primary text-light ">
-            <div class="card-subtitle align-middle">
-                <h4 class="mb-0">{{ user.displayName }}</h4>
-            </div>
-        </div>
-            <div class="card-body">
+            <div class="card-title align-middle">
+                <h5 class="mb-0">{{ user.displayName }} Mini</h5>
             </div>
           </div>
+          <div class="card-body">
+              <div class="text--primary align-middle">
+                <b class="mb-0">{{ user.userName }}</b>
+              </div>
+              <div class="card-text align-middle">
+                <i class="mb-0">{{ user.bio }}</i>
+              </div>
+          </div>
+          </div>
+        </div> 
+        </div>
+        <!-- Full Card -->
+        <div class="col-sm-12 col-md-6 col-lg-4" v-else>
+          <div>
+          <div class="card card-block rounded shadow shadow-sm">
+            <div class="card-header bg-primary text-light ">
+            <div class="card-title align-middle">
+                <h5 class="mb-0">{{ user.displayName }} Mini</h5>
+            </div>
+          </div>
+          <div class="card-body">
+              <div class="text--primary align-middle">
+                <b class="mb-0">{{ user.userName }}</b>
+              </div>
+              <div class="card-text align-middle">
+                <i class="mb-0">{{ user.bio }}</i>
+              </div>
+          </div>
+          </div>
+        </div> 
         </div>
       </div>
     </div>
@@ -19,7 +49,8 @@
   import { withDefaults, defineProps } from "vue"
   import {User} from "@/types/UserTypes";
 interface Props{
-      user: User
+      user: User,
+      isMiniCard : boolean
   }
     const props = withDefaults(defineProps<Props>(),{
       user : ()=>{
@@ -29,6 +60,7 @@ interface Props{
           bio: 'Hello, I am Default User',
         }
       },
+      isMiniCard : true
     });
   
 </script>
