57c4e421f9e3aaaf0c3f859be90a52b57416f55b
Anu Viswan <a.viswan@kruss.de>
2024-05-07 12:12:55 +0530

    Updated UI to include checks for UserName Update Props from ViewProfile.vue

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/user/UserProfileCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/user/UserProfileCard.vue
@@ -1,73 +1,72 @@
 <template>
- <div class="container mt-5 d-flex justify-content-center">
+  <div class="container mt-5 d-flex justify-content-center">
 
-<div class="card p-3 shadow">
+    <div class="card p-3 shadow">
 
-    <div class="d-flex align-items-center">
-
+      <div class="d-flex align-items-center">
         <div class="image">
-    <img src="@/assets/user.png" class="rounded" width="155" >
-    </div>
-
-    <div class="ml-3 w-100">
-        
-       <h4 class="mb-0 mt-0">{{ user.displayName }}</h4>
-       <span>{{ user.userName }}</span>
-       <p class="bio">{{ user.bio }}</p>
-
-       <div class="p-2 mt-2 bg-secondary d-flex justify-content-between rounded text-white stats">
-
-        <div class="d-flex flex-column">
-
-            <span class="stat-heading">Followers<i class="bi bi-people"></i></span>
-            <span class="number1">{{ getValueOrDefault(user.countOfFollowers) }}</span>
-            
-        </div>
-
-        <div class="d-flex flex-column">
-
-            <span class="stat-heading">Following</span>
-            <span class="number2">{{ getValueOrDefault(user.countOfFollowedUsers) }}</span>
-            
+          <img src="@/assets/user.png" class="rounded" width="155">
         </div>
 
 
-        <div class="d-flex flex-column">
-            <span class="stat-heading">Rating</span>
-            <span class="number3">{{ getValueOrDefault(user.Downrated) }}</span>
+        <div class="ml-3 w-100">
+          <h4 class="mb-0 mt-0">{{ user.displayName }}</h4>
+          <span>{{ user.userName }}</span>
+          <p class="bio">{{ user.bio }}</p>
+
+          <!-- Stats -->
+          <div class="p-2 mt-2 bg-secondary d-flex justify-content-between rounded text-white stats">
+            <div class="d-flex flex-column">
+              <span class="stat-heading">Followers<i class="bi bi-people"></i></span>
+              <span class="number1">{{ getValueOrDefault(user.countOfFollowers) }}</span>
+            </div>
+
+            <div class="d-flex flex-column">
+              <span class="stat-heading">Following</span>
+              <span class="number2">{{ getValueOrDefault(user.countOfFollowedUsers) }}</span>
+            </div>
+
+            <div class="d-flex flex-column">
+              <span class="stat-heading">Rating</span>
+              <span class="number3">{{ getValueOrDefault(user.Downrated) }}</span>
+            </div>
+
+          </div>
+
+          <!--End User Stats-->
+
+          <!-- Buttons -->
+
+          <div class="button mt-2 d-flex flex-row align-items-center">
+            <button class="btn btn-sm btn-primary w-100 ml-2" :disabled="!canFollow()">{{ followText() }}</button>
+          </div>
+
+          <!--End Buttons-->
+
+
         </div>
 
-        
-           
-       </div>
 
-
-       <div class="button mt-2 d-flex flex-row align-items-center">
-
-        <button class="btn btn-sm btn-outline-primary w-100">Chat</button>
-        <button class="btn btn-sm btn-primary w-100 ml-2">Follow</button>
-
-           
-       </div>
-
+      </div>
 
     </div>
 
-        
-    </div>
-    
-</div>
- 
-</div>
+  </div>
 </template>
 <script setup lang="ts">
-import {computed } from 'vue'
+import {ref} from 'vue';
 import { withDefaults, defineProps } from "vue"
 import { User } from "@/types/UserTypes";
+import {useUserStore } from '@/stores/userStore';
+
 interface Props {
   user: User,
   isMiniCard: boolean
 }
+
+const store = useUserStore();
+const currentUserName = ref(store.UserName);
+
 const props = withDefaults(defineProps<Props>(), {
   user: () => {
     return {
@@ -79,11 +78,22 @@ const props = withDefaults(defineProps<Props>(), {
   isMiniCard: true
 });
 
-const getValueOrDefault = (val:number|undefined) => val === null || val === undefined ? 0: val;
+const getValueOrDefault = (val: number | undefined) => val === null || val === undefined ? 0 : val;
+
+function canFollow():boolean{
+  console.log('Props:'+ props.user.userName)
+  console.log('Store:'+ currentUserName.value)
+  return props.user.userName != currentUserName.value;
+}
+
+function followText():string{
+
+  console.log(canFollow());
+  return canFollow()?"Follow": "Following";
+}
 </script>
 <style scoped>
-
-.card{
+.card {
   width: 400px;
   border: 10;
   border-radius: 10px;
@@ -92,30 +102,32 @@ const getValueOrDefault = (val:number|undefined) => val === null || val === unde
 
 
 
-.stats{
+.stats {
 
-      background: #f2f5f8 !important;
+  background: #f2f5f8 !important;
 
-    color: #000 !important;
+  color: #000 !important;
 }
 
-.bio{
-  font-size:12px;
+.bio {
+  font-size: 12px;
   font-style: italic;
 }
-.stat-heading{
-  font-size:10px;
+
+.stat-heading {
+  font-size: 10px;
   color: #6a6b6e;
 }
-.number1{
-  font-weight:500;
+
+.number1 {
+  font-weight: 500;
 }
 
-.number2{
-  font-weight:500;
+.number2 {
+  font-weight: 500;
 }
 
-.number3{
-  font-weight:500;
+.number3 {
+  font-weight: 500;
 }
 </style>
--- client/nt.webclient/vue3withtypescript/nt/src/pages/private/user/ViewProfile.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/pages/private/user/ViewProfile.vue
@@ -1,10 +1,20 @@
 <template>
  <div class="container-fluid">
-      <UserProfileCard/>
+      <UserProfileCard :user="currentUser"/>
     </div>
 </template>
 <script setup lang="ts">
+import {ref} from 'vue';
 import UserProfileCard from "@/components/private/user/UserProfileCard.vue";
+import {useUserStore } from '@/stores/userStore';
+import { User } from '@/types/UserTypes';
+
+const store = useUserStore();
+const currentUser = ref<User>({
+  userName : store.UserName,
+  displayName : store.DisplayName,
+  bio : store.Bio,
+})
 </script>
 
 <style scoped>
