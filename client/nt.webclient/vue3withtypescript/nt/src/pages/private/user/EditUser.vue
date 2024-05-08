1389d260235320ac81c8cd8979031c6de4c910c9
Anu Viswan <a.viswan@kruss.de>
2024-05-08 10:16:04 +0530

    Adding ui,

--- client/nt.webclient/vue3withtypescript/nt/src/pages/private/user/EditUser.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/pages/private/user/EditUser.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="container-fluid">
-        <div class="card rounded shadow shadow-sm">
-            <div class="row">
-                <div class="card-body bg-light text-uppercase">
-                    <div class="card-title">
-                        <h5 class="mb-0" align-middle>Edit User</h5>
+        <div class="row row-cols-2">
+            <div class="col col-4 col-xl-2 align-self-start">
+                <div class="card rounded shadow shadow-sm">
+            <div class="card-body bg-light text-uppercase">
+                    <div class="card-title text-left">
+                        <h5 class="mb-0" align-middle>Photo</h5>
                     </div>
                 </div>
-            </div>
-
             <div class="card-body">
-
                 <form class="form needs-validation" @submit.prevent="onSubmit">
-                    <div class="form-group text-start">
+                    <div class="form-group text-left">
                         <label>UserName</label>
                         <input type="text" v-model="formData.userName" class="form-control block" placeholder="Username" />
+                    </div>
+                    <div class="d-flex justify-content-left" v-if="v$.formData.userName.$error">
+                        <ValidationMessage :messages="v$.formData.userName.$errors.map((x: any) => x.$message)"
+                            v-bind:isError="true" />
+                    </div>
+                    <div class="form-group text-left">
+                        <label>Display Name</label>
+                        <input type="text" v-model="formData.userName" class="form-control block" placeholder="Display Name" />
+                    </div>
+                    <div class="form-group text-left">
+                        <label>About yourself</label>
+                        <input type="text" v-model="formData.userName" class="form-control block" placeholder="Display Name" />
                     </div>
                     <div class="d-flex justify-content-left" v-if="v$.formData.userName.$error">
                         <ValidationMessage :messages="v$.formData.userName.$errors.map((x: any) => x.$message)"
@@ -32,6 +42,52 @@
 
             </div>
         </div>
+            </div>
+
+            <div class="col col-8 col-xl-8 align-self-end">
+                <div class="card rounded shadow shadow-sm">
+            <div class="card-body bg-light text-uppercase">
+                    <div class="card-title text-left">
+                        <h5 class="mb-0" align-middle>Edit User</h5>
+                    </div>
+                </div>
+            <div class="card-body">
+                <form class="form needs-validation" @submit.prevent="onSubmit">
+                    <div class="form-group text-left">
+                        <label>UserName</label>
+                        <input type="text" v-model="formData.userName" class="form-control block" placeholder="Username" />
+                    </div>
+                    <div class="d-flex justify-content-left" v-if="v$.formData.userName.$error">
+                        <ValidationMessage :messages="v$.formData.userName.$errors.map((x: any) => x.$message)"
+                            v-bind:isError="true" />
+                    </div>
+                    <div class="form-group text-left">
+                        <label>Display Name</label>
+                        <input type="text" v-model="formData.userName" class="form-control block" placeholder="Display Name" />
+                    </div>
+                    <div class="form-group text-left">
+                        <label>About yourself</label>
+                        <input type="text" v-model="formData.userName" class="form-control block" placeholder="Display Name" />
+                    </div>
+                    <div class="d-flex justify-content-left" v-if="v$.formData.userName.$error">
+                        <ValidationMessage :messages="v$.formData.userName.$errors.map((x: any) => x.$message)"
+                            v-bind:isError="true" />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-block btn-primary" value="Submit" />
+                    </div>
+
+                    <div class="d-flex justify-content-left" v-if="v$.serverMessage.$error">
+                        <ValidationMessage :messages="v$.serverMessage.$errors.map((x: any) => x.$message)"
+                            v-bind:isError="true" />
+                    </div>
+                </form>
+
+            </div>
+        </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 <script setup lang="ts">
