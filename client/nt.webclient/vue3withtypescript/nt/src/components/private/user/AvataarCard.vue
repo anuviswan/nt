92d93b8708a0939a6bd72e3686360ed3e9b4f4a7
Anu Viswan <a.viswan@kruss.de>
2024-05-10 12:19:22 +0530

    Added preview image

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/user/AvataarCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/user/AvataarCard.vue
@@ -1,15 +1,54 @@
 <template>
-    <input id="profile-image-upload" class="d-none" type="file">
+    <input id="profile-image-upload" ref="fileUploader" class="d-none" type="file" @change="handleImageChanged">
     <div class="card">
-        
-        <img src="https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg"
-        class="avataar image-center  rounded-circle card-img img-thumbnail mx-auto d-block" alt="Profile Image">
+        <img :src="imgSrc" 
+        class="avataar image-center  rounded-circle card-img img-thumbnail mx-auto d-block" 
+        alt="Profile Image">
         <div class="card-img-overlay d-flex">
-            <div class="align-self-center mx-auto"><i class="fas fa-file-upload fa-2x fileupload"></i></div>
+            <div class="align-self-center mx-auto">
+                <i class="fas fa-file-upload fa-2x fileupload" @click="browseImage()"></i>
+            </div>
         </div>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { def } from '@vue/shared';
+import {onMounted, ref} from 'vue'
+
+const fileUploader = ref<HTMLInputElement|null>(null);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires    
+const defaultImage = require('@/assets/DefaultProfile.jpg')
+const imgSrc = ref<string>(defaultImage);
+
+onMounted(()=>{
+    imgSrc.value = defaultImage;
+})
+
+const browseImage = ():void=>{
+    if(fileUploader?.value == null) return;
+    fileUploader!.value!.click();
+    return;
+}
+
+const handleImageChanged = (e:Event) => {
+    const file = (e.target as HTMLInputElement)!.files?.[0];
+    
+    if(file){
+
+        console.log(file);
+        const reader = new FileReader();
+        
+
+        reader.onload = (e)=>{
+            imgSrc.value = e.target?.result as string;
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
+
+</script>
 <style scoped>
     .avataar{
         vertical-align: middle;
