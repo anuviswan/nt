9dbeda4dabd294899c7713a0ba989be209bb6031
Anu Viswan <a.viswan@kruss.de>
2024-05-15 16:15:21 +0530

    creating skeleton code for CQR for uploading file

---
+++ server/nt.microservice/services/UserService/UserService.Service/Command/UploadImageCommand.cs
@@ -0,0 +1,18 @@
+﻿using UserService.Service.Dtos;
+
+namespace UserService.Service.Command;
+
+public class UploadImageCommand:IRequest<ProfileImageDto>
+{
+    public required string UserName { get; set; }
+    public required string ImageKey { get; set; }
+    public required MemoryStream FileData { get; set; }
+}
+
+public class UploadImageCommandHandler : IRequestHandler<UploadImageCommand, ProfileImageDto>
+{
+    public Task<ProfileImageDto> Handle(UploadImageCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ server/nt.microservice/services/UserService/UserService.Service/Dtos/ProfileImageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Service.Dtos;
+
+public record ProfileImageDto
+{
+    public string ImageKey { get; set; }
+    public string UserName { get; set; }
+    public MemoryStream FileData { get; set; }
+}
