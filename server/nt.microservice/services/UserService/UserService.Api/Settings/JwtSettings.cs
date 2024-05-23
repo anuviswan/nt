6c7aeb6f79894464972efb1d46aa08f4c6c4898e
Anu Viswan <anu.viswan@gmail.com>
2024-05-23 21:36:34 +0530

    moved to own file

---
+++ server/nt.microservice/services/UserService/UserService.Api/Settings/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace UserService.Api.Settings;
+
+public record JwtSettings
+{
+    public string Key { get; init; } = string.Empty;
+    public string Issuer { get; init; } = string.Empty;
+    public string Aud { get; init; } = string.Empty;
+
+    public bool Validate()
+    {
+        return Key is not null &&
+            Issuer is not null &&
+            Aud is not null;
+    }
+}
--- server/nt.microservice/services/UserService/UserService.Api/Settings/RabbitMqSettings.cs
+++ server/nt.microservice/services/UserService/UserService.Api/Settings/RabbitMqSettings.cs
@@ -6,17 +6,3 @@ public record RabbitMqSettings
     public string UserName { get; init; } = string.Empty;    
     public string Password { get; init; } = string.Empty;
 }
-
-public record JwtSettings
-{
-    public string Key { get; init; } = string.Empty;
-    public string Issuer { get; init; } = string.Empty;
-    public string Aud { get; init; } = string.Empty;
-
-    public bool Validate()
-    {
-        return Key is not null &&
-            Issuer is not null &&
-            Aud is not null;
-    }
-}
