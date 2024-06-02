0c9811c98bdeacba8e8b2526f0eae5921a9acd03
Anu Viswan <anu.viswan@gmail.com>
2024-06-02 07:54:23 +0530

    Added unit test cases for SearchUserByDisplayNameQueryHandler

--- server/nt.microservice/services/UserService/UserService.Service/Query/SearchUserByDisplayNameQueryHandler.cs
+++ server/nt.microservice/services/UserService/UserService.Service/Query/SearchUserByDisplayNameQueryHandler.cs
@@ -1,22 +1,24 @@
-﻿using UserService.Service.Dtos;
+﻿using AutoMapper;
+using UserService.Service.Dtos;
 
 namespace UserService.Service.Query;
 
 public class SearchUserByDisplayNameQueryHandler : IRequestHandler<SearchUserByDisplayNameQuery, IEnumerable<UserProfileDto>>
 {
     private readonly IUserMetaInformationRepository _userMetaInformationRepository;
-    public SearchUserByDisplayNameQueryHandler(IUserMetaInformationRepository userMetaInformationRepository)
+    private readonly IMapper _mapper;
+    public SearchUserByDisplayNameQueryHandler(IUserMetaInformationRepository userMetaInformationRepository,IMapper mapper)
     {
         _userMetaInformationRepository = userMetaInformationRepository;
+        _mapper = mapper;
     }
     public async Task<IEnumerable<UserProfileDto>> Handle(SearchUserByDisplayNameQuery request, CancellationToken cancellationToken)
     {
-        ArgumentNullException.ThrowIfNull(request?.QueryPart);
-        var userMetaInfo = await _userMetaInformationRepository.SearchUserByPartialDisplayName(request.QueryPart).ConfigureAwait(false);
-        return userMetaInfo.Select(x=> new UserProfileDto
-        {
-            UserName = x.UserName,
-            DisplayName = x.DisplayName,
-        });
+        ArgumentNullException.ThrowIfNullOrEmpty(request?.QueryPart);
+        var users = await _userMetaInformationRepository.SearchUserByPartialDisplayName(request.QueryPart).ConfigureAwait(false);
+
+        if(!users.Any()) return Enumerable.Empty<UserProfileDto>();
+
+        return _mapper.Map<IEnumerable<UserProfileDto>>(users);
     }
 }
