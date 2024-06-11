7f32073476cfa3792e8b204e4547ee19929bfee2
Anu Viswan <anu.viswan@gmail.com>
2024-06-11 21:12:51 +0530

    Adding test cases for ValueInjector test cases

--- server/nt.microservice/services/MovieService/MovieService.Domain/Entities/Movie.cs
+++ server/nt.microservice/services/MovieService/MovieService.Domain/Entities/Movie.cs
@@ -2,7 +2,7 @@
 
 public class Movie
 {
-    public string? MovieTitle { get; set; }
+    public string? Title { get; set; }
     public string? Language { get; set; }
     public DateOnly? ReleaseDate { get; set; }
 }
