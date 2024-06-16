83e47649384abb78ab9d4c96bcd20d74cdd5edfd
Anu Viswan <anu.viswan@gmail.com>
2024-06-16 21:29:52 +0530

    renamed and moved files

--- server/nt.microservice/services/MovieService/MovieService.Api.Tests/ControllerTests/MovieControllerTests/CreateMovieTests.cs
+++ server/nt.microservice/services/MovieService/MovieService.Api.Tests/ControllerTests/MovieControllerTests/CreateMovieTests.cs
@@ -5,13 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MovieService.Api.Tests.ControllerTests;
+namespace MovieService.Api.Tests.ControllerTests.MovieControllerTests;
 
 [TestClass]
-public class MovieControllerTests : ControllerTestsBase
+public class CreateMovieTests : ControllerTestsBase
 {
     public Task CreateMovie_ValidMovieTitle_Success(CreateMovieRequest createMovieRequest, CreateMovieResponse expectedResults)
     {
-        
+        return Task.CompletedTask;
     }
 }
