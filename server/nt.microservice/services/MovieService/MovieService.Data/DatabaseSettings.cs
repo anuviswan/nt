4b9fc151f6bf2bf872a58f81f9cb7a047f189ce1
Anu Viswan <anu.viswan@gmail.com>
2024-09-10 22:50:16 +0530

    Added Database Settings Moved Entity to Domain Layer

---
+++ server/nt.microservice/services/MovieService/MovieService.Data/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieService.Data;
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string MovieCollectionName { get; set; } = null!;
+}
--- server/nt.microservice/services/MovieService/MovieService.Domain/Entities/Movie.cs
+++ server/nt.microservice/services/MovieService/MovieService.Domain/Entities/Movie.cs
@@ -1,8 +1,23 @@
-﻿namespace MovieService.Domain.Entities;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MovieService.Domain.Entities;
 
 public class Movie
 {
-    public string? Title { get; set; }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("title")]
+    public string Title { get; set; } = null!;
+
+    [BsonElement("language")]
     public string? Language { get; set; }
+
+    [BsonElement("release-date")]
     public DateOnly? ReleaseDate { get; set; }
+
+    [BsonElement("director")]
+    public string? Director { get; set; }
 }
