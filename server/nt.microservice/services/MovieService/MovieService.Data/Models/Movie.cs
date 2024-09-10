b7b70a4d53bb0b211c4dcfb3dac731b0073b7310
Anu Viswan <a.viswan@kruss.de>
2024-09-10 20:29:46 +0530

    added movie entity

---
+++ server/nt.microservice/services/MovieService/MovieService.Data/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MovieService.Data.Models;
+
+public class Movie
+{
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("title")]
+    public string Title { get; set; }
+
+
+    [BsonElement("release-date")]
+    public DateOnly? ReleaseDate { get; set; }
+
+
+    [BsonElement("director")]
+    public string? Directory { get; set; } 
+}
