16add24d41a60868f640e779337a4a2d9b05a21b
Anu Viswan <a.viswan@kruss.de>
2024-09-20 16:00:04 +0530

    Adding GraphQlModels

---
+++ server/nt.microservice/services/MovieService/MovieService.Api/GraphQL/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieService.Api.GraphQL.Models;
+
+public record Movie
+{
+    public string Title { get; set; }
+    public string Language { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
