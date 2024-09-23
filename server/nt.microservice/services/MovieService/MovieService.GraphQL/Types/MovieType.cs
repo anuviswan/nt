e9792f0a2bbd86f527c8aa74091b2012b3c12dff
Anu Viswan <anu.viswan@gmail.com>
2024-09-23 21:11:53 +0530

    Working on types

--- server/nt.microservice/services/MovieService/MovieService.GraphQL/Types/MovieType.cs
+++ server/nt.microservice/services/MovieService/MovieService.GraphQL/Types/MovieType.cs
@@ -7,11 +7,17 @@ namespace MovieService.GraphQL.Types
     {
         protected override void Configure(IObjectTypeDescriptor<MovieDto> descriptor)
         {
-            base.Configure(descriptor);
+            descriptor.Description("Defines a movie");
 
-            descriptor.Field(x=>x.Title).Type<StringType>();
-            descriptor.Field(x=>x.Language).Type<StringType>();
-            descriptor.Field(x=>x.ReleaseDate).Type<DateTimeType>();
+            descriptor.Field(x=>x.Title)
+                      .Type<StringType>()
+                      .Description("Title of Movie");
+            descriptor.Field(x=>x.Language)
+                      .Type<StringType>()
+                      .Description("Language of movie");
+            descriptor.Field(x=>x.ReleaseDate)
+                      .Type<DateTimeType>()
+                      .Description("Release date of movie");
         }
     }
 }
