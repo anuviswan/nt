16fb968d65743b41528a7266c4b349e7fc27f660
Anu Viswan <anu.viswan@gmail.com>
2024-09-28 14:23:57 +0530

    added IGraphType

--- client/nt.webclient/vue3withtypescript/nt/src/types/apirequestresponsetypes/Response.ts
+++ client/nt.webclient/vue3withtypescript/nt/src/types/apirequestresponsetypes/Response.ts
@@ -1,6 +1,9 @@
-import { IResponseBase } from '@/types/apirequestresponsetypes/Response';
 export interface IResponseBase {
   hasError: boolean;
   status?: number;
   errors?: Array<string>;
+}
+
+export interface IGraphQlType{
+
 }
