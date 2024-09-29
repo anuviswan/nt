7c3553d1fd28a04e6914634c20fca131ba71186e
Anu Viswan <anu.viswan@gmail.com>
2024-09-29 14:51:51 +0530

    working on crew card

---
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/movie/CrewsCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Person } from '@/types/MovieTypes';
+
+
+interface Props{
+    crewTitle:string,
+    crewMembers: Person[]|undefined
+}
+
+const props = withDefaults(defineProps<Props>(),{
+    crewTitle : 'Director',
+    crewMemers: ['ac','def']
+});
+
+console.log(props)
+</script>
+<template>
+    <div>
+        <div>asdasd</div>
+        {{ crewTitle }}
+<div class="col" v-for="(person,index) in crewMembers" :key="index">{{person.name}}</div>
+</div>
+</template>
--- client/nt.webclient/vue3withtypescript/nt/src/components/private/movie/MovieCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/movie/MovieCard.vue
@@ -1,21 +1,48 @@
 <script setup lang="ts">
-  import { Movie } from '@/types/MovieTypes';
+  import { Movie, Person } from '@/types/MovieTypes';
+  import CrewsCard from './CrewsCard.vue';
   interface Props {
     movie: Movie;
   }
   const props = defineProps<Props>();
 
-  console.log('movie props ' + props.movie);
+
+  function findCrewMembers(currentMovie:Movie,key:string):Person[]|undefined{
+    if(currentMovie.crew)
+  {
+    console.log(currentMovie)
+    console.log(currentMovie.crew)
+    return currentMovie.crew.find(crewMember => crewMember.key === key)?.value;
+  }
+  else
+  return undefined;
+  }
+
+
 </script>
 <template>
-  <div class="card p-3 shadow">
-    <div class="d-flex align-items-center">
-      <div class="ml-3 w-100">
-        <h4 class="mb-0 mt-0">{{ movie.title }}</h4>
-        <p class="small">({{ movie.language }})</p>
+ <div class="card p-3 shadow">
+  <div class="d-flex align-items-center">
+    <div class="ml-3 w-100">
+      <h4 class="mb-0 mt-0">{{ movie.title }}</h4>
+      <p class="small">({{ movie.language }})</p>
+    </div>
+  </div>
+
+  <!-- Director Section Below Language -->
+  <div class="row mt-3">
+    <div class="col">
+      <div class="row" v-if="findCrewMembers(movie,'Director')">
+        <div class="col">Director1</div>
+        <div class="col" v-for="(director,index) in findCrewMembers(movie,'Director')" :key="index">{{director.name}}</div>
+      </div>
+      <div class="row">
+        <CrewsCard :crew-members="['a','b','c']" crew-title="sasa"/>
       </div>
     </div>
   </div>
+</div>
+
 </template>
 
 <style scoped>
