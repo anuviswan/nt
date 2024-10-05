69570bafb5e64efc3a2ccc5bdd5656a850efb93a
Anu Viswan <anu.viswan@gmail.com>
2024-10-05 11:32:12 +0530

    fixed responses

--- client/nt.webclient/vue3withtypescript/nt/src/types/apirequestresponsetypes/Movie.ts
+++ client/nt.webclient/vue3withtypescript/nt/src/types/apirequestresponsetypes/Movie.ts
@@ -1,7 +1,31 @@
-import { Movie } from '@/types/MovieTypes';
 import {  IGraphQlResponseBase } from '@/types/apirequestresponsetypes/Response';
 
 export interface ISearchMoviesResponse extends IGraphQlResponseBase {
-  findMovie: Movie[]
+  findMovie: MovieResponse[]
   
 }
+
+export interface MovieResponse {
+  title: string;
+  language: string;
+  releaseDate: Date;
+  cast: CastResponse;
+  crew: KeyValuePair<string,PersonResponse[]>[];
+}
+
+export interface CastResponse{
+  edges : PersonEdgeResponse[]
+}
+export interface PersonEdgeResponse{
+  node: PersonResponse
+}
+
+export interface KeyValuePair<TKey,TValue> {
+  key: TKey;
+  value: TValue;
+}
+
+export interface PersonResponse {
+  name: string;
+}
+
