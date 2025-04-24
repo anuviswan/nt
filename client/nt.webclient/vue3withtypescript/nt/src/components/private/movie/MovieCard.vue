b07771c2977cf5a62b0f4c0370d40afd47f69992
Anu Viswan <anu.viswan@gmail.com>
2025-04-24 05:53:40 +0530

    date formating

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/movie/MovieCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/movie/MovieCard.vue
@@ -18,6 +18,15 @@
   return undefined;
   }
 
+function formatDate(date: string | Date): string {
+  const parsedDate = typeof date === 'string' ? new Date(date) : date;
+
+  return parsedDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+}
 
 </script>
 <template>
@@ -25,7 +34,8 @@
   <div class="d-flex align-items-center">
     <div class="ml-3 w-100">
       <h4 class="mb-0 mt-0">{{ movie.title }}</h4>
-      <p class="small">({{ movie.language }})</p>
+      <p class="small">({{ movie.movieLanguage }})</p>
+      <p class="small">Released on : {{ formatDate(movie.releaseDate) }}</p>
     </div>
   </div>
 
