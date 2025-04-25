6f68f450ac07022ba0159f4aeea3060a3e817b93
Anu Viswan <anu.viswan@gmail.com>
2025-04-25 19:49:57 +0530

    Renamed property

--- server/nt.microservice/services/MovieService/MovieService.Api/ViewModels/CreateMovieRequest.cs
+++ server/nt.microservice/services/MovieService/MovieService.Api/ViewModels/CreateMovieRequest.cs
@@ -5,7 +5,7 @@ namespace MovieService.Api.ViewModels;
 public record CreateMovieRequest
 {
     public required string Title { get; init; }
-    public required string Language { get; init; }
+    public required string MovieLanguage { get; init; }
 
     [TechnicalDebt(DebtType.MissingFeature, "Missing support for DateOnly in MongoDb, will use DateTime until then")]
     public required DateTime? ReleaseDate { get; init; }
