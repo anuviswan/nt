9e9b31aea6a84b90f076f0024b47058291ab57bf
Anu Viswan <anu.viswan@gmail.com>
2025-05-07 08:24:10 +0530

    Fixed UserIdentityAggregatorService

--- server/nt.microservice/aggregatorservices/UserIdentityAggregatorService/UserIdentityAggregatorService.Api/Controllers/UserController.cs
+++ server/nt.microservice/aggregatorservices/UserIdentityAggregatorService/UserIdentityAggregatorService.Api/Controllers/UserController.cs
@@ -1,8 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UserIdentityAggregatorService.Api.Services.AuthService;
+using UserIdentityAggregatorService.Api.Services.UserService;
+using UserIdentityAggregatorService.Api.ViewModels.ValidateUser;
 
 namespace UserIdentityAggregatorService.Api.Controllers;
 
+[ApiController]
+[Route("api/user")]
 public class UserController : ControllerBase
 {
+    private readonly AuthService _authService;
+    private readonly UserService _userService;
+    private readonly ILogger<UserController> _logger;
 
+    public UserController(UserService userService, AuthService authService, ILogger<UserController> logger)
+    {
+        _userService = userService;
+        _authService = authService;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [Route("Validate")]
+    public async Task<ActionResult<ValidateUserResponseViewModel>> ValidateUser(ValidateUserRequestViewModel request)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var authenticateResponse = await _authService.ValidateAsync(new AuthorizeRequestViewModel
+            {
+                UserName = request.userName,
+                PassKey = request.passKey
+            });
+
+            if (!authenticateResponse.IsAuthenticated)
+                return Unauthorized();
+
+            var userDetails = await _userService.SearchUserByUserNameAsync(new SearchUserByUserNameRequestViewModel { UserName = request.userName });
+
+            return Ok(new ValidateUserResponseViewModel
+            {
+                IsAuthenticated = true,
+                Token = authenticateResponse.Token,
+                LoginTime = authenticateResponse.LoginTime,
+                UserName = authenticateResponse.UserName,
+                Bio = userDetails.User.Bio,
+                DisplayName = userDetails.User.DisplayName,
+            });
+        }
+        catch (Services.AuthService.ApiException apiex) when (apiex.StatusCode == 401)
+        {
+            return Unauthorized();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return BadRequest(ex.Message);
+        }
+    }
 }
---
+++ server/nt.microservice/aggregatorservices/UserIdentityAggregatorService/UserIdentityAggregatorService.Api/ViewModels/ValidateUser/ValidateUserResponseViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace UserIdentityAggregatorService.Api.ViewModels.ValidateUser;
+
+
+public record ValidateUserResponseViewModel
+{
+    public required string Token { get; set; }
+
+    public required bool IsAuthenticated { get; set; }
+
+    public required DateTime LoginTime { get; set; }
+
+    public required string UserName { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public string? Bio { get; set; }
+}
