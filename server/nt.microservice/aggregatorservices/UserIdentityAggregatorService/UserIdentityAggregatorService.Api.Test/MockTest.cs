e9bd57ee0ba7226ae659fb71eec43c5e71387d1f
Anu Viswan <anu.viswan@gmail.com>
2025-05-08 21:45:21 +0530

    Added mock test to ensure atleast one run

---
+++ server/nt.microservice/aggregatorservices/UserIdentityAggregatorService/UserIdentityAggregatorService.Api.Test/MockTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserIdentityAggregatorService.Api.Test;
+
+[TestClass]
+public class MockTest
+{
+    [TestMethod]
+    public void MockTestMethod()
+    {
+        Assert.IsTrue(true);
+    }
+}
