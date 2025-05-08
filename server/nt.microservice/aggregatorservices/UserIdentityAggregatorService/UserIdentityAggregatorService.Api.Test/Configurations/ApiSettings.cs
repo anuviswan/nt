99f3587c02b32172f4ce4c93884b8684f972fc96
Anu Viswan <anu.viswan@gmail.com>
2025-05-08 21:19:16 +0530

    Added Test Project for UserIdentityAggregatorService.Api

---
+++ server/nt.microservice/aggregatorservices/UserIdentityAggregatorService/UserIdentityAggregatorService.Api.Test/Configurations/ApiSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace UserIdentityAggregatorService.Api.Test.Configurations;
+
+internal record ApiSetting
+{
+    public required string Key { get; set; }
+    public required string Uri { get; set; }
+    public string? FileName { get; set; }
+    public string? ClassName { get; set; }
+    public string? Namespace { get; set; }
+}
