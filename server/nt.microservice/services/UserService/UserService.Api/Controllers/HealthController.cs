905370b6bbcbfb62a011c76b6d4cfc2e729e0c84
Anu Viswan <anu.viswan@gmail.com>
2025-05-11 07:18:43 +0530

    register user service with consul

---
+++ server/nt.microservice/services/UserService/UserService.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿
+namespace UserService.Api.Controllers;
+
+[ApiController]
+[Route("api/healthcheck")]
+public class HealthController : BaseController
+{
+    public HealthController(IMediator mediator, IMapper mapper, ILogger<HealthController> logger) : base(mediator, mapper, logger)
+    {
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Route("health")]
+    public ActionResult Health()
+    {
+        return Ok();
+    }
+}
