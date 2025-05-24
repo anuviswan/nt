ab09beea374425889f017d40507b7e0060b2b8e0
Anu Viswan <anu.viswan@gmail.com>
2025-05-24 07:58:41 +0530

    Changed Consul endpoint to http

--- server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
+++ server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
@@ -5,7 +5,7 @@ builder.AddUserIdentityAggregatorService();
 
 var consul = builder.AddContainer("consul", "hashicorp/consul:latest")
     .WithContainerName("nt.common.servicediscovery")
-    .WithEndpoint(port:9500, targetPort:8500)
+    .WithHttpEndpoint(port:9500, targetPort:8500)
     .WithArgs("agent", "-dev", "-client=0.0.0.0"); // dev mode
 
 builder.AddAuthService()
