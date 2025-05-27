bc1636c4a3e46dd8ad97946c2ba7dc98443e0bfe
Anu Viswan <anu.viswan@gmail.com>
2025-05-27 22:04:38 +0530

    fixed rabbitmq health check

--- server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
+++ server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
@@ -1,3 +1,7 @@
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
+var root = @"D:\Source\nt\server\nt.microservice";
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddGateway();
@@ -9,17 +13,26 @@ var consul = builder.AddContainer("consul", "hashicorp/consul:latest")
     .WithArgs("agent", "-dev", "-client=0.0.0.0"); // dev mode
 
 
-builder.AddAuthService(consul)
+var rabbitMqusername = builder.AddParameter("rabbitMqusername", "ntuser", secret: true);
+var rabbitMqpassword = builder.AddParameter("rabbitMqpassword", "pass", secret: true);
+
+var rabbitmq = builder.AddRabbitMQ("nt-common-rabbitmq", rabbitMqusername, rabbitMqpassword)
+    .WithBindMount(source: @$"{root}/services/transportservices/rabbitmq-enabled-plugins",
+                   target: @"/etc/rabbitmq/enabled_plugins")
+    .WithBindMount(source: @$"{root}/services/transportservices/rabbitmq.config",
+                   target: @"/etc/rabbitmq/rabbitmq.conf")
+    .WithBindMount(source: @$"{root}/services/transportservices/rabbitmq-defs.json",
+                   target: @"/etc/rabbitmq/definitions.json")
+    .WithHttpEndpoint(5672,targetPort:5672, name:"http1")
+    .WithHttpEndpoint(15672,targetPort:15672, name: "http2");
+
+
+builder.AddAuthService(consul, rabbitmq)
        .WaitFor(consul);
 
 //builder.AddMovieService();
-builder.AddUserService()
+builder.AddUserService(rabbitmq)
        .WaitFor(consul);
-
-//builder.AddReviewService();
-
-
-
 
 //builder.AddReviewService();
 
