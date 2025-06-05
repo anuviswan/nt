dadefa6fab5f5bb4f0f3dfd35b3392211d459027
Anu Viswan <anu.viswan@gmail.com>
2025-06-05 06:53:37 +0530

    removed IResourceBuilderExtensions cleanup

--- server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
+++ server/nt.microservice/infrastructure/nt.orchestrator.AppHost/Program.cs
@@ -1,9 +1,3 @@
-using Aspire.Hosting;
-using Aspire.Hosting.ApplicationModel;
-using Grpc.Net.Client.Balancer;
-using static nt.helper.Constants.Infrastructure;
-
-
 var root = @"D:\Source\nt\server\nt.microservice";
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -29,6 +23,13 @@ var rabbitmq = builder.AddRabbitMQ(Constants.Infrastructure.RabbitMq.ServiceName
 
 
 
+
+
+
+
+
+
+
 var postgresUsername = builder.AddParameter("postgresUser", "postgres", secret: true);
 var postgresPassword = builder.AddParameter("postgressPassword", "Admin123", secret: true);
 
@@ -38,6 +39,29 @@ var postgres = builder.AddPostgres("UserSqlDb", postgresUsername, postgresPasswo
             .WithPgAdmin()
             .WithDataVolume()
             .WithInitBindMount(@"D:\Source\nt\server\nt.microservice\services\Db\scripts\Aspire");
+
+var mongoDbUsername = builder.AddParameter("mongoDbUser", "root", secret: true);
+var mongoDbPassword = builder.AddParameter("mongoDbPassword", "mypass", secret: true);
+
+var mongoDb = builder.AddMongoDB("nt-movieservice-db", 27017, userName: mongoDbUsername, password: mongoDbPassword)
+            .WithEnvironment("MONGO_INITDB_ROOT_USERNAME", "root")
+            .WithEnvironment("MONGO_INITDB_ROOT_PASSWORD", "mypass")
+            //.WithEndpoint(port: 27017, targetPort: 27017, isProxied: true)
+            .WithContainerName("nt.movieservice.db")
+            .WithDataVolume()
+            .WithMongoExpress();
+
+var blobStorage = builder.AddContainer("nt-userservice-blobstorage", "mcr.microsoft.com/azure-storage/azurite")
+            //    .WithVolume(@"./localstorage/data:/data")
+            .WithArgs("azurite-blob", "--blobHost", "0.0.0.0", "-l", "/data")
+            .WithHttpEndpoint(port: 10000, targetPort: 10000, isProxied: true);
+
+
+var sqlServerPassword = builder.AddParameter("sqlServerPassword", "Admin123");
+var sqlDb = builder.AddSqlServer("nt-userservice-db", sqlServerPassword)
+            .WithEnvironment("ACCEPT_EULA", "Y")
+            .WithEnvironment("MSSQL_SA_PASSWORD", "Admin123")
+            .WithHttpEndpoint(port: 1433, targetPort: 1433, isProxied: true);
 
 var authServiceInstances = new List<IResourceBuilder<ProjectResource>>();
 for (int port = 8101; port <= 8105; port++)
@@ -82,25 +106,39 @@ var aggregatorService = builder.AddProject<Projects.UserIdentityAggregatorServic
             .WithEnvironment("ServiceDiscoveryOptions__Services__1__Name", "nt.authservice.loadbalancer")
             .WaitFor(consulServiceDiscovery);
 
-builder.AddProject<Projects.nt_gateway>(Constants.Gateway.ServiceName, launchProfileName: Constants.Gateway.LaunchProfile)
+var gateway = builder.AddProject<Projects.nt_gateway>(Constants.Gateway.ServiceName, launchProfileName: Constants.Gateway.LaunchProfile)
            .WithEnvironment(Constants.Global.EnvironmentVariables.RunningWithVariable, Constants.Global.EnvironmentVariables.RunningWithValue)
            .WithEnvironment(Constants.Global.EnvironmentVariables.HostVariable, Constants.Global.EnvironmentVariables.HostValue);
 
 
-builder.AddUserService(rabbitmq)
-        .WithEnvironment("ConsulConfig__serviceName", "nt.userservice.service")
-        .WithEnvironment("ConsulConfig__serviceId", "userservice-1")
-        .WithEnvironment("ConsulConfig__serviceAddress", $"localhost")
-        .WithEnvironment("ConsulConfig__servicePort", "8301")
-        .WithEnvironment("ConsulConfig__healthCheckUrl", "http://host.docker.internal:8301/api/healthcheck/health")
-        .WithEnvironment("ConsulConfig__consulAddress", consulServiceDiscovery.GetEndpoint("http"))
-        .WithEnvironment("ConsulConfig__deregisterAfterMinutes", "5")
-        .WaitFor(consulServiceDiscovery)
-        .WithUrls(c => c.Urls.ForEach(u => u.DisplayText = $"Open API ({u.Endpoint?.EndpointName})")); 
+var userService = builder.AddProject<Projects.UserService_Api>("nt-userservice-service")
+            .WithEnvironment("RUNNING_WITH", "aspire")
+            .WithEnvironment("RabbitMqSettings__host", "localhost")
+            .WithEnvironment("RabbitMqSettings__username", "ntuser")
+            .WithEnvironment("RabbitMqSettings__password", "pass")
+            .WithEnvironment("ConsulConfig__serviceName", "nt.userservice.service")
+            .WithEnvironment("ConsulConfig__serviceId", "userservice-1")
+            .WithEnvironment("ConsulConfig__serviceAddress", $"localhost")
+            .WithEnvironment("ConsulConfig__servicePort", "8301")
+            .WithEnvironment("ConsulConfig__healthCheckUrl", "http://host.docker.internal:8301/api/healthcheck/health")
+            .WithEnvironment("ConsulConfig__consulAddress", consulServiceDiscovery.GetEndpoint("http"))
+            .WithEnvironment("ConsulConfig__deregisterAfterMinutes", "5")
+            .WithHttpEndpoint(8301, name: "http")
+            .WaitFor(blobStorage)
+            .WithReference(sqlDb)
+            .WaitFor(sqlDb)
+            .WaitFor(consulServiceDiscovery)
+            .WithUrls(c => c.Urls.ForEach(u => u.DisplayText = $"Open API ({u.Endpoint?.EndpointName})"));
 
 
-builder.AddMovieService();
-//builder.AddReviewService();
+var movieService = builder.AddProject<Projects.MovieService_Api>("nt-movieservice-service")
+        .WithEnvironment("MovieDatabase__DatabaseName", "ntmoviestore")
+        .WithEnvironment("MovieDatabase__MovieCollectionName", "movies")
+        .WithReference(mongoDb)
+        .WaitFor(mongoDb);
+
+var reviewService = builder.AddProject<Projects.ReviewService_Api>("nt-reviewservice-service")
+        .WithUrls(c => c.Urls.ForEach(u => u.DisplayText = $"Open API ({u.Endpoint?.EndpointName})"));
 
 
 builder.Build().Run();
