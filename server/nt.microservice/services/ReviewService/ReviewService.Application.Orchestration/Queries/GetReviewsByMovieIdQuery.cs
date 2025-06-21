d34e5f8964ddc062482a6d8f5663b0bdf148ebe7
Anu Viswan <anu.viswan@gmail.com>
2025-06-21 22:07:46 +0530

    working on get review by movieid

---
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Orchestration/Queries/GetReviewsByMovieIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ReviewService.Application.DTO.Reviews;
+
+namespace ReviewService.Application.Orchestration.Queries;
+
+public class GetReviewsByMovieIdQuery : IRequest<IEnumerable<Review>>
+{
+    public Guid MovieId { get; set; }
+    public GetReviewsByMovieIdQuery(Guid movieId)
+    {
+        MovieId = movieId;
+    }
+}
---
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Orchestration/Queries/GetReviewsByMovieIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using ReviewService.Application.DTO.Reviews;
+using ReviewService.Application.Interfaces.Operations;
+
+namespace ReviewService.Application.Orchestration.Queries;
+
+public class GetReviewsByMovieIdQueryHandler : IRequestHandler<GetReviewsByMovieIdQuery, IEnumerable<Review>>
+{
+    private readonly IReviewService _reviewService;
+    public GetReviewsByMovieIdQueryHandler(IReviewService reviewService)
+    {
+        _reviewService = reviewService;
+    }
+
+    public Task<IEnumerable<Review>> Handle(GetReviewsByMovieIdQuery request, CancellationToken cancellationToken)
+    {
+        return _reviewService.GetReviewsByMovieIdAsync(request.MovieId);
+    }
+}
