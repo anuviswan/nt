85acb8f193d35fee11b8b589f46dca50037ef55a
Anu Viswan <anu.viswan@gmail.com>
2025-06-22 07:48:12 +0530

    adding review service

---
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Services/Operations/ReviewService.cs
@@ -0,0 +1,12 @@
+﻿using ReviewService.Application.DTO.Reviews;
+using ReviewService.Application.Services.Interfaces;
+
+namespace ReviewService.Application.Services.Operations;
+
+public class ReviewService : IReviewService
+{
+    public Task<IEnumerable<Review>> GetReviewsByMovieIdAsync(Guid movieId)
+    {
+        throw new NotImplementedException();
+    }
+}
