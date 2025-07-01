afe662d92d49dc84bc6d04d6c83bc0de786003f8
Anu Viswan <anu.viswan@gmail.com>
2025-07-01 21:58:24 +0530

    Added Review service Moved Repository interfaces to domain

--- server/nt.microservice/services/ReviewService/ReviewService.Application.Services/Operations/ReviewService.cs
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Services/Operations/ReviewService.cs
@@ -1,17 +1,39 @@
-﻿using ReviewService.Application.DTO.Reviews;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using ReviewService.Application.DTO.Reviews;
 using ReviewService.Application.Interfaces.Operations;
+using ReviewService.Domain.Entities;
+using ReviewService.Domain.Repositories;
 
 namespace ReviewService.Application.Services.Operations;
 
 public class ReviewService : IReviewService
 {
+    private readonly IReviewRepository _reviewRepository;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
+    public ReviewService(IReviewRepository reviewRepository,IMapper mapper, ILogger<ReviewService> logger)
+    {
+        _reviewRepository = reviewRepository ?? throw new ArgumentNullException(nameof(reviewRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
     public Task<IEnumerable<ReviewDto>> GetReviewsByMovieIdAsync(Guid movieId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<Guid> CreateReviewAsync(ReviewDto reviewDto)
+    public async Task<Guid> CreateReviewAsync(ReviewDto reviewDto)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var review = await _reviewRepository.AddAsync(_mapper.Map<ReviewDto, Review>(reviewDto));
+            return review.Id;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while creating a review.");
+            throw;
+        }
     }
 }
---
+++ server/nt.microservice/services/ReviewService/ReviewService.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using ReviewService.Domain.Entities;
+
+namespace ReviewService.Domain.Repositories;
+
+public interface IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+{
+    Task<IEnumerable<TEntity>> GetAll();
+    Task<TEntity> GetByIdAsync(long id);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync(TEntity entity);
+}
