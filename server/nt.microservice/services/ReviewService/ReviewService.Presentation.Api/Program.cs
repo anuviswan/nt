d4d891f7f5b6737a4c25451b74e7b9ee2b491f22
Anu Viswan <anu.viswan@gmail.com>
2025-07-13 18:21:57 +0530

    adding mongo to review service

--- server/nt.microservice/services/ReviewService/ReviewService.Presentation.Api/Program.cs
+++ server/nt.microservice/services/ReviewService/ReviewService.Presentation.Api/Program.cs
@@ -1,9 +1,3 @@
-
-using Couchbase;
-using Microsoft.Extensions.Options;
-using ReviewService.Presenation.Api.HostedServices;
-using ReviewService.Presenation.Api.Options;
-
 namespace ReviewService.Api;
 
 public class Program
@@ -14,19 +8,6 @@ public class Program
         builder.AddServiceDefaults();
 
         
-        builder.Services.Configure<CouchbaseSettings>(builder.Configuration.GetSection(nameof(CouchbaseSettings)));
-
-        //builder.Services.AddSingleton<ICluster>(sp =>
-        //{
-        //    var options = sp.GetRequiredService<IOptions<CouchbaseSettings>>().Value;
-        //    var cluster = Cluster.ConnectAsync(options.ConnectionString, options.Username, options.Password)
-        //                         .GetAwaiter().GetResult();
-        //    Cluster.ConnectAsync()
-        //    return cluster;
-        //});
-        builder.Services.AddSingleton<IClusterProvider, LazyClusterProvider>();
-        builder.Services.AddHostedService<ClusterBootstrapService>();
-        builder.Services.AddHostedService<EnsureBucketService>();
 
         // Add services to the container.
         builder.Services.AddControllers();
@@ -52,32 +33,3 @@ public class Program
 }
 
 
-
-
-public interface IClusterProvider
-{
-    Task<ICluster> GetClusterAsync();
-}
-
-public class LazyClusterProvider : IClusterProvider
-{
-    private readonly CouchbaseSettings _settings;
-    private ICluster _cluster;
-
-    public LazyClusterProvider(IOptions<CouchbaseSettings> settings)
-    {
-        _settings = settings.Value;
-    }
-
-    public async Task<ICluster> GetClusterAsync()
-    {
-        if (_cluster != null) return _cluster;
-
-        _cluster = await Cluster.ConnectAsync(
-            _settings.ConnectionString,
-            _settings.Username,
-            _settings.Password);
-
-        return _cluster;
-    }
-}
