90d7502c38e85c9f8e2e78dff5da5d76da05707f
Anu Viswan <anu.viswan@gmail.com>
2025-07-16 22:32:20 +0530

    working on seeds

--- server/nt.microservice/services/ReviewService/ReviewService.Infrastructure.Repository/Seed/MalayalamReviewsSeed.cs
+++ server/nt.microservice/services/ReviewService/ReviewService.Infrastructure.Repository/Seed/MalayalamReviewsSeed.cs
@@ -4,11 +4,15 @@ namespace ReviewService.Infrastructure.Repository.Seed;
 
 public static class MalayalamReviewsSeed
 {
-    public static IEnumerable<ReviewDocument> Reviews => [];
-}
-
-public static class Seed
-{
-    public static IEnumerable<ReviewDocument> MalayalamReviews => MalayalamReviewsSeed.Reviews;
-    
+    public static IEnumerable<ReviewDocument> Reviews => [
+        new(){
+            Content = "Super movie",
+            ID  = Guid.Parse("d1f8c2b0-3e4b-4c5a-9f6b-7a8b9c0d1e2f"),,
+            MovieId = Guid.Parse("6191e634-14c8-45d1-898f-191060cdbec1"),
+            Rating = 5,
+            ReviewId = Guid.Parse("a1b2c3d4-e5f6-7a8b-9c0d-1e2f3a4b5c6d"),
+            Title = "Super Movie",
+            UserName = "jia.anu"
+        }
+        ];
 }
---
+++ server/nt.microservice/services/ReviewService/ReviewService.Infrastructure.Repository/Seed/Seed.cs
@@ -0,0 +1,9 @@
+﻿using ReviewService.Infrastructure.Repository.Documents;
+
+namespace ReviewService.Infrastructure.Repository.Seed;
+
+public static class Seed
+{
+    public static IEnumerable<ReviewDocument> MalayalamReviews => MalayalamReviewsSeed.Reviews;
+    
+}
