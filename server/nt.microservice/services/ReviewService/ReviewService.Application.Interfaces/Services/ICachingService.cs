a8c9f876a3ddabb85d75d9d5bc9363008e4108e1
Anu Viswan <anu.viswan@gmail.com>
2025-07-22 22:20:57 +0530

    adding caching service

---
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Interfaces/Services/ICachingService.cs
@@ -0,0 +1,7 @@
+﻿namespace ReviewService.Application.Interfaces.Services;
+
+public interface ICachingService
+{
+    void Set<T>(string key, T value, TimeSpan expirationTime) where T : class;
+    T? Get<T>(string key) where T : class;
+}
---
+++ server/nt.microservice/services/ReviewService/ReviewService.Application.Services/Services/CachingService.cs
@@ -0,0 +1,20 @@
+﻿using ReviewService.Application.Interfaces.Services;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace ReviewService.Application.Services.Services;
+
+public class CachingService(IConnectionMultiplexer connectionMultiplexer) : ICachingService
+{
+    public IConnectionMultiplexer ConnectionMultiplexer => connectionMultiplexer;
+    public IDatabase Database => ConnectionMultiplexer.GetDatabase();
+    public void Set<T>(string key, T value, TimeSpan expirationTime) where T : class
+    {
+        Database.StringSet(key, JsonSerializer.Serialize(value), expirationTime);
+    }
+    public T? Get<T>(string key) where T : class
+    {
+        var value = Database.StringGet(key);
+        return value.IsNullOrEmpty ? null : JsonSerializer.Deserialize<T>(value!);
+    }
+}
