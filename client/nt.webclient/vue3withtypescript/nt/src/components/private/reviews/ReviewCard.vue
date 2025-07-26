dddcadcc467cfe3da7894aaa3c4f8b2a425d4925
Anu Viswan <anu.viswan@gmail.com>
2025-07-27 01:58:04 +0530

    Creating ReviewCard

---
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/ReviewCard.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { Review } from '@/types/ReviewTypes';
+
+
+
+interface Props{
+    review:Review
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">
+                {{ review.title }}
+            </div>
+            <div class="card-subtitle">
+                {{ review.movieTitle }}
+            </div>
+            <div class="card-body">
+                {{ review.content }}
+            </div>
+        </div>
+    </div>
+</template>
