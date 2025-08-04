25907a1344b818b0bc444519648b3b1ecf5c6ba3
Anu Viswan <anu.viswan@gmail.com>
2025-08-04 21:45:07 +0530

    working on ui

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/ReviewCard.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/ReviewCard.vue
@@ -17,14 +17,14 @@
           <img
             src="https://t4.ftcdn.net/jpg/02/12/52/91/360_F_212529193_YRhcQCaJB9ugv5dFzqK25Uo9Ivm7B9Ca.jpg"
             style="width: 200px"
-            class="img-fluid rounded-start h-100 object-fit-cover"
+            class="img-fluid rounded-start h-80 object-fit-cover"
             alt="Movie Poster"
           />
         </div>
 
         <!-- Content Section -->
         <div class="col-md-10">
-          <div class="card-body">
+          <div class="card-body d-flex flex-column">
             <h5 class="card-title text-primary">{{ review.title }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">
               {{ review.movieTitle }}
@@ -34,7 +34,10 @@
               {{ review.content }}
             </p>
 
-            <div class="d-flex justify-content-between align-items-center mt-4">
+            <!-- Stick this section to the bottom -->
+            <div
+              class="d-flex justify-content-between align-items-center mt-auto"
+            >
               <div class="d-flex align-items-center">
                 <span class="badge bg-warning text-dark"
                   >⭐ {{ review.rating }}</span
