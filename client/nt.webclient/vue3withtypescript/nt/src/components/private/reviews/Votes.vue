77d1f3ef9a513d0b474fde898cd3a4590742cc69
Anu Viswan <anu.viswan@gmail.com>
2025-08-16 21:58:49 +0530

    voter class

--- client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/Votes.vue
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/Votes.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
-  import { defineProps } from 'vue';
+  import { defineProps, withDefaults, computed } from 'vue';
   interface Props {
     IsUpvote: boolean;
     Count: number;
     Text: string;
+    IsSelected: boolean;
   }
 
-  const props = defineProps<Props>();
+  const props = withDefaults(defineProps<Props>(), {
+    IsUpvote: true,
+    Text: 'Upvoted',
+    Count: 32,
+    IsSelected: false,
+  });
+
+  const iconClass = computed(() => {
+    if (props.IsUpvote) {
+      return props.IsSelected
+        ? 'bi bi-hand-thumbs-up-fill'
+        : 'bi bi-hand-thumbs-up';
+    } else {
+      return props.IsSelected
+        ? 'bi bi-hand-thumbs-down-fill'
+        : 'bi bi-hand-thumbs-down';
+    }
+  });
 </script>
 <template>
-  <div></div>
+  <div class="d-flex align-items-center text-muted">
+    <i :class="iconClass" class="me-2"></i>
+    <span>{{ Count }} {{ Text }}</span>
+  </div>
 </template>
