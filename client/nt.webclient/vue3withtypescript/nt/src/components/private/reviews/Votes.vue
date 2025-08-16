f67ab47758ad9d5a800527f3ffff35ea3319cce7
Anu Viswan <anu.viswan@gmail.com>
2025-08-16 21:40:09 +0530

    creating voting control

---
+++ client/nt.webclient/vue3withtypescript/nt/src/components/private/reviews/Votes.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { defineProps } from 'vue';
+  interface Props {
+    IsUpvote: boolean;
+    Count: number;
+    Text: string;
+  }
+
+  const props = defineProps<Props>();
+</script>
+<template>
+  <div></div>
+</template>
